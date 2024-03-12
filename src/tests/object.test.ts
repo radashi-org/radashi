1e17b0042b2751dfaefbc0ac5b25b83b46cfb70d
UnKnoWn <9733856+UnKnoWn-Consortium@users.noreply.github.com>
2024-03-12 11:40:29 +0800

    fix(object): fixed `set` not setting keys starting with number correctly

--- src/tests/object.test.ts
+++ src/tests/object.test.ts
@@ -496,14 +496,28 @@ describe('object module', () => {
       expect(_.set({}, 'cards.0.value', 2)).toEqual({
         cards: [{ value: 2 }]
       })
+      expect(_.set({}, 'cards.2.value', 2)).toEqual({
+        cards: [, , { value: 2 }]
+      })
       expect(_.set({}, 'cards.0.0.value', 2)).toEqual({
         cards: [[{ value: 2 }]]
+      })
+      expect(_.set({}, 'cards.2.2.value', 2)).toEqual({
+        cards: [, , [, , { value: 2 }]]
       })
       expect(_.set({}, 'cards.[0].[0].value', 2)).toEqual({
         cards: [[{ value: 2 }]]
       })
       expect(_.set({}, 'cards.[1].[1].value', 2)).toEqual({
         cards: [, [, { value: 2 }]]
+      })
+    })
+    test('sets keys starting with numbers correctly', () => {
+      expect(_.set({}, 'cards.0value', 2)).toEqual({
+        cards: { '0value': 2 }
+      })
+      expect(_.set({}, 'cards.1234value', 2)).toEqual({
+        cards: { '1234value': 2 }
       })
     })
   })
