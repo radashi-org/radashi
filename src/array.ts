081bc21fc02949f10497aa23f0af24c6ab6b6043
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-23 17:10:12 -0400

    feat(select): let `condition` be undefined

--- src/array.ts
+++ src/array.ts
@@ -221,12 +221,13 @@ export const objectify = <T, Key extends string | number | symbol, Value = T>(
 export const select = <T, K>(
   array: readonly T[],
   mapper: (item: T, index: number) => K,
-  condition: (item: T, index: number) => boolean
+  condition?: (item: T, index: number) => boolean
 ) => {
   if (!array) return []
+  let mapped: K
   return array.reduce((acc, item, index) => {
-    if (!condition(item, index)) return acc
-    acc.push(mapper(item, index))
+    if (condition) condition(item, index) && acc.push(mapper(item, index))
+    else (mapped = mapper(item, index)) != null && acc.push(mapped)
     return acc
   }, [] as K[])
 }
