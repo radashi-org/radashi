71ed7c8e224ea0fe95a4ed76fb350b711ac11f2b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-24 19:36:58 -0400

    chore: give each function its own module
    …while preserving the commit history!

--- src/random/uid.ts
+++ src/random/uid.ts
@@ -1,31 +1,5 @@
-import { iterate } from './array'
-
-/**
- * Generates a random number between min and max
- */
-export const random = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-/**
- * Draw a random item from a list. Returns
- * null if the list is empty
- */
-export const draw = <T>(array: readonly T[]): T | null => {
-  const max = array.length
-  if (max === 0) {
-    return null
-  }
-  const index = random(0, max - 1)
-  return array[index]
-}
-
-export const shuffle = <T>(array: readonly T[]): T[] => {
-  return array
-    .map(a => ({ rand: Math.random(), value: a }))
-    .sort((a, b) => a.rand - b.rand)
-    .map(a => a.value)
-}
+import { iterate } from '../array/iterate'
+import { random } from './random'
 
 export const uid = (length: number, specials: string = '') => {
   const characters =
