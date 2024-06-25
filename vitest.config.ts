60b38d141819076b1de07da64b4711d93e80250c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-25 00:13:26 -0400

    chore: add resolve.alias to vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vitest/config'
 
+const resolve = (specifier: string) =>
+  new URL(import.meta.resolve(specifier)).pathname
+
 export default defineConfig({
   test: {
     globals: true,
     coverage: {
       thresholds: { 100: true }
+    }
+  },
+  resolve: {
+    alias: {
+      radashi: resolve('./src/index.js')
     }
   }
 })
