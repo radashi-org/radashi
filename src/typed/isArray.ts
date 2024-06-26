88c12b6e3941b1fa6072b9ec9bf214c508e7bb70
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-25 22:44:02 -0400

    fix(isArray): work with `readonly T[]` types
    This change was manually tested with the following code:
    
    ```ts
    import { isArray } from 'radashi'
    
    declare const a: number | number[]
    declare const b: string | readonly string[]
    
    if (Array.isArray(a)) a.forEach(item => { /* ✅ item is number */ })
    if (Array.isArray(b)) b.forEach(item => { /* 👎 item is any */ })
    
    if (isArray(a)) a.forEach(item => { /* ✅ item is number */ })
    if (isArray(b)) b.forEach(item => { /* ✅ item is string */ })
    ```

--- src/typed/isArray.ts
+++ src/typed/isArray.ts
@@ -1,1 +1,4 @@
-export const isArray = Array.isArray
+// The function wrapper exists to ensure `isArray` can be tree-shaked.
+// Its performance impact is negligible.
+export const isArray = /* @__PURE__ */ (() =>
+  Array.isArray as (value: unknown) => value is readonly any[])()
