ddfe1dccf458272789f35b9896516775e66fba6e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-25 23:28:47 -0400

    test: add matchKeys tests

---
+++ src/curry/tests/matchKeys.test.ts
@@ -0,0 +1,44 @@
+import * as _ from 'radashi'
+
+describe('matchKeys function', () => {
+  const objectEntries = Object.entries as <T extends object>(
+    obj: T
+  ) => [keyof T, T[keyof T]][]
+
+  test('accepts an array of keys', () => {
+    const keys = ['a', 'b']
+    const matches = _.matchKeys(keys)
+    const obj = {
+      a: 1,
+      b: 2,
+      c: 3
+    }
+    for (const [key, value] of objectEntries(obj)) {
+      expect(matches(value, key, obj)).toBe(keys.includes(key))
+    }
+  })
+  test('accepts a filter callback', () => {
+    const matches = _.matchKeys(
+      (value, key) => value !== undefined || key === 'd'
+    )
+    const obj = {
+      a: 1,
+      b: undefined,
+      c: 3,
+      d: undefined
+    }
+    for (const [key, value] of objectEntries(obj)) {
+      expect(matches(value, key, obj)).toBe(value !== undefined || key === 'd')
+    }
+  })
+  test('does not match non-enumerable keys', () => {
+    const matches = _.matchKeys(['a', 'b'])
+    class A {
+      a = 1
+      b() {}
+    }
+    const a = new A()
+    expect(matches(a.a, 'a', a)).toBe(true)
+    expect(matches(a.b, 'b', a)).toBe(false)
+  })
+})
