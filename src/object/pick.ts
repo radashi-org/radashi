ffd79fe1ec6c59cb3684a84c15b66761aa186bf1
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-26 13:54:42 -0400

    feat(pick): accept a callback for advanced picking

--- src/object/pick.ts
+++ src/object/pick.ts
@@ -1,20 +1,20 @@
+import { type FilteredKeys, type KeyFilter, reduceKeys } from "radashi";
+
 /**
  * Pick a list of properties from an object into a new object
  */
-export function pick<T extends object, TKeys extends keyof T>(
-  obj: T,
-  keys: TKeys[],
-): Pick<T, TKeys> {
-  if (!obj) {
-    return {} as Pick<T, TKeys>
-  }
-  return keys.reduce(
-    (acc, key) => {
-      if (Object.hasOwnProperty.call(obj, key)) {
-        acc[key] = obj[key]
-      }
-      return acc
-    },
-    {} as Pick<T, TKeys>,
-  )
+export function pick<T extends object, F extends KeyFilter<T, keyof T>>(
+	obj: T,
+	filter: F,
+): Pick<T, FilteredKeys<T, F>> {
+	if (!obj) return {} as any;
+	return reduceKeys(
+		obj,
+		filter,
+		(acc, value, key) => {
+			acc[key] = value;
+			return acc;
+		},
+		{} as any,
+	);
 }
