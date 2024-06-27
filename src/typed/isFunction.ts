6ad96f44f17949ee33acda9a073307b19dca7796
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-27 17:54:34 -0400

    fix: use typeof in `isFunction`
    There‘s no clear benefit to using duck typing, so avoid any weird edge cases by sticking to what‘s tried and true: the `typeof` operator.

--- src/typed/isFunction.ts
+++ src/typed/isFunction.ts
@@ -1,4 +1,4 @@
 // biome-ignore lint/complexity/noBannedTypes:
 export function isFunction(value: any): value is Function {
-  return !!(value?.constructor && value.call && value.apply)
+  return typeof value === 'function'
 }
