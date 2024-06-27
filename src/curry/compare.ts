67b8c323adfc571752f324a63093ce95a6bf153e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-27 17:46:44 -0400

    feat(compare): add optional `compare` callback

--- src/curry/compare.ts
+++ src/curry/compare.ts
@@ -12,24 +12,43 @@ export type ComparableProperty<T> = {
   [P in keyof T]: T[P] extends Comparable ? P : never
 }[keyof T]
 
-export type ComparedValue<T> =
+export type ComparedBy<T> =
   | (T extends object ? ComparableProperty<T> : never)
   | ((arg: T) => Comparable)
 
-export function compare<PropertyKey extends keyof any>(
-  prop: PropertyKey
+export type ComparedValue<T, By extends ComparedBy<T>> = T extends (
+  arg: any,
+) => infer Result
+  ? Result
+  : T[By & keyof T]
+
+export function compare<
+  PropertyKey extends keyof any,
+  Value extends Comparable = Comparable,
+>(
+  property: PropertyKey,
+  compare?: (a: Value, b: Value) => number,
 ): (
-  a: { [P in PropertyKey]: Comparable },
-  b: { [P in PropertyKey]: Comparable }
+  a: { [P in PropertyKey]: Value },
+  b: { [P in PropertyKey]: Value },
 ) => number
 
-export function compare<T>(prop: ComparedValue<T>): (a: T, b: T) => number
+export function compare<T, By extends ComparedBy<T>>(
+  by: By,
+  compare?: (a: ComparedValue<T, By>, b: ComparedValue<T, By>) => number,
+): (a: T, b: T) => number
 
-export function compare(arg: keyof any | ((obj: any) => any)) {
-  const select = isFunction(arg) ? arg : (obj: any) => obj[arg]
+export function compare(
+  by: keyof any | ((obj: any) => any),
+  compare?: (a: any, b: any) => number,
+) {
+  const select = isFunction(by) ? by : (obj: any) => obj[by]
   return (left: any, right: any) => {
     const leftValue = select(left)
     const rightValue = select(right)
+    if (compare) {
+      return compare(leftValue, rightValue)
+    }
     return leftValue > rightValue ? 1 : leftValue < rightValue ? -1 : 0
   }
 }
