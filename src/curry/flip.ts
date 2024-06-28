f6401acf54555fc0ae6458b0a2a07d44458a0d45
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-27 20:39:10 -0400

    chore: format

--- src/curry/flip.ts
+++ src/curry/flip.ts
@@ -1,5 +1,5 @@
 export function flip<Arg1, Arg2, Result>(
-  fn: (arg1: Arg1, arg2: Arg2) => Result
+  fn: (arg1: Arg1, arg2: Arg2) => Result,
 ): (arg2: Arg2, arg1: Arg1) => Result {
   return (arg2, arg1) => fn(arg1, arg2)
 }
