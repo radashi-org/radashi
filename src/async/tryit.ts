f0443644bace43ad3092751e0ba4193ead336ef6
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-30 00:49:07 -0400

    feat(types): add `TryitResult<T>` type

--- src/async/tryit.ts
+++ src/async/tryit.ts
@@ -1,34 +1,38 @@
 import { isPromise } from 'radashi'
 
 /**
+ * The result of a `tryit` function.
+ *
+ * If the function returns a promise, the result is a promise that
+ * resolves to an error-first callback-_like_ array response as
+ * `[Error, result]`.
+ *
+ * If the function returns a non-promise, the result is an error-first
+ * callback-_like_ array response as `[Error, result]`.
+ */
+export type TryitResult<Return> = Return extends Promise<any>
+  ? Promise<[Error, undefined] | [undefined, Awaited<Return>]>
+  : [Error, undefined] | [undefined, Return]
+
+/**
  * A helper to try an async function without forking the control flow.
  * Returns an error-first callback-_like_ array response as `[Error,
  * result]`
  */
 export function tryit<Args extends any[], Return>(
   func: (...args: Args) => Return,
-) {
-  return (
-    ...args: Args
-  ): Return extends Promise<any>
-    ? Promise<[Error, undefined] | [undefined, Awaited<Return>]>
-    : [Error, undefined] | [undefined, Return] => {
+): (...args: Args) => TryitResult<Return> {
+  return (...args) => {
     try {
       const result = func(...args)
       if (isPromise(result)) {
         return result
           .then(value => [undefined, value])
-          .catch(err => [err, undefined]) as Return extends Promise<any>
-          ? Promise<[Error, undefined] | [undefined, Awaited<Return>]>
-          : [Error, undefined] | [undefined, Return]
+          .catch(err => [err, undefined]) as TryitResult<Return>
       }
-      return [undefined, result] as Return extends Promise<any>
-        ? Promise<[Error, undefined] | [undefined, Awaited<Return>]>
-        : [Error, undefined] | [undefined, Return]
+      return [undefined, result] as TryitResult<Return>
     } catch (err) {
-      return [err as any, undefined] as Return extends Promise<any>
-        ? Promise<[Error, undefined] | [undefined, Awaited<Return>]>
-        : [Error, undefined] | [undefined, Return]
+      return [err, undefined] as TryitResult<Return>
     }
   }
 }
