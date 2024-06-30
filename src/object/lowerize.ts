96b28b9b037bd03277511d0174e4896729bcee93
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-30 00:54:54 -0400

    feat(types): export `UppercaseKeys` and `LowercaseKeys` types
    These types are used by `upperize` and `lowerize` respectively.

--- src/object/lowerize.ts
+++ src/object/lowerize.ts
@@ -1,12 +1,14 @@
 import { mapKeys } from 'radashi'
 
-type LowercasedKeys<T extends Record<string, any>> = {
+export type LowercaseKeys<T extends Record<string, any>> = {
   [P in keyof T & string as Lowercase<P>]: T[P]
 }
 
 /**
  * Convert all keys in an object to lower case
  */
-export function lowerize<T extends Record<string, any>>(obj: T) {
-  return mapKeys(obj, k => k.toLowerCase()) as LowercasedKeys<T>
+export function lowerize<T extends Record<string, any>>(
+  obj: T,
+): LowercaseKeys<T> {
+  return mapKeys(obj, k => k.toLowerCase()) as LowercaseKeys<T>
 }
--- src/object/upperize.ts
+++ src/object/upperize.ts
@@ -1,12 +1,14 @@
 import { mapKeys } from 'radashi'
 
-type UppercasedKeys<T extends Record<string, any>> = {
+export type UppercaseKeys<T extends Record<string, any>> = {
   [P in keyof T & string as Uppercase<P>]: T[P]
 }
 
 /**
  * Convert all keys in an object to upper case
  */
-export function upperize<T extends Record<string, any>>(obj: T) {
-  return mapKeys(obj, k => k.toUpperCase()) as UppercasedKeys<T>
+export function upperize<T extends Record<string, any>>(
+  obj: T,
+): UppercaseKeys<T> {
+  return mapKeys(obj, k => k.toUpperCase()) as UppercaseKeys<T>
 }
