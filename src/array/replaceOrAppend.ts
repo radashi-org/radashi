d052f2eee520986ee81e0b97aeb18c8fede07970
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-02 14:59:27 -0400

    chore: format

--- src/array/replaceOrAppend.ts
+++ src/array/replaceOrAppend.ts
@@ -28,7 +28,7 @@ export function replaceOrAppend<T>(
   const out = array.slice()
   for (let index = 0; index < array.length; index++) {
     if (match(array[index], index)) {
-      out[index] =  newItem;
+      out[index] = newItem
       return out
     }
   }
