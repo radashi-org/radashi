f7f3848463ddc0b924a7af43db3779bd1eaaf39f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-02 18:34:57 -0400

    chore: use prettier on readme & docs

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,7 +4,8 @@ set -e
 UNCOMMITTED_CHANGES=$(git status --porcelain -uno)
 
 # 2. Update the formatting.
-biome check --fix --unsafe
+pnpm biome check --fix --unsafe
+pnpm prettier --write README.md docs/**/*
 
 # 3. Commit if there were no uncommitted changes, but there are now.
 if [ -z "$UNCOMMITTED_CHANGES" -a -z "$(git status --porcelain -uno)" ]; then
