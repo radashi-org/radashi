cafc7fc4833447d6e5ed6bdc88957201aae6372a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-03 15:40:19 -0400

    fix: `toInt` and `toFloat` should not throw on symbols (#67)

--- tests/number/toFloat.test.ts
+++ tests/number/toFloat.test.ts
@@ -2,23 +2,28 @@ import * as _ from 'radashi'
 
 describe('toFloat', () => {
   test('handles null', () => {
-    const result = _.toFloat(null)
-    expect(result).toBe(0.0)
+    expect(_.toFloat(null)).toBe(0.0)
   })
   test('handles undefined', () => {
-    const result = _.toFloat(undefined)
-    expect(result).toBe(0.0)
+    expect(_.toFloat(undefined)).toBe(0.0)
   })
   test('uses null default', () => {
-    const result = _.toFloat('x', null)
-    expect(result).toBeNull()
+    expect(_.toFloat('x', null)).toBeNull()
   })
   test('handles bad input', () => {
-    const result = _.toFloat({})
-    expect(result).toBe(0.0)
+    expect(_.toFloat({})).toBe(0.0)
   })
-  test('converts 20.00 correctly', () => {
-    const result = _.toFloat('20.00')
-    expect(result).toBe(20.0)
+  test('do not throw on symbols', () => {
+    expect(_.toFloat(Symbol())).toBe(0)
+  })
+  test('convert "12.34" to 12.34', () => {
+    expect(_.toFloat('12.34')).toBe(12.34)
+  })
+  test('preserve infinite values', () => {
+    expect(_.toFloat(Number.POSITIVE_INFINITY)).toBe(Number.POSITIVE_INFINITY)
+    expect(_.toFloat(Number.NEGATIVE_INFINITY)).toBe(Number.NEGATIVE_INFINITY)
+  })
+  test('convert NaN to default', () => {
+    expect(_.toFloat(Number.NaN, 1)).toBe(1)
   })
 })
