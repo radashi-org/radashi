c0f94e2e284275551805c55a61fa0d0b0fbca03b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-03 17:19:00 -0400

    fix: better isArray return type

--- src/typed/isArray.ts
+++ src/typed/isArray.ts
@@ -1,2 +1,9 @@
-export const isArray: (value: unknown) => value is readonly any[] =
-  Array.isArray
+export const isArray = Array.isArray as <Input>(
+  value: Input,
+) => value is readonly any[] extends Extract<Input, readonly any[]>
+  ? Extract<Input, readonly any[]>
+  : any[] extends Extract<Input, any[]>
+    ? Extract<Input, any[]>
+    : unknown[] extends Input
+      ? unknown[]
+      : never
---
+++ tests/typed/isArray.test-d.ts
@@ -0,0 +1,44 @@
+import * as _ from 'radashi'
+
+describe('isArray return type', () => {
+  test('value is unknown', () => {
+    const value = {} as unknown
+    if (_.isArray(value)) {
+      expectTypeOf(value).toEqualTypeOf<unknown[]>()
+    } else {
+      expectTypeOf(value).toEqualTypeOf<unknown>()
+    }
+  })
+  test('value is string', () => {
+    const value = {} as string
+    if (_.isArray(value)) {
+      expectTypeOf(value).toEqualTypeOf<never>()
+    } else {
+      expectTypeOf(value).toEqualTypeOf<string>()
+    }
+  })
+  test('value is string or ReadonlyMap', () => {
+    const value = {} as string | readonly string[]
+    if (_.isArray(value)) {
+      expectTypeOf(value).toEqualTypeOf<readonly string[]>()
+    } else {
+      expectTypeOf(value).toEqualTypeOf<string>()
+    }
+  })
+  test('value is string, ReadonlyMap, or Map', () => {
+    const value = {} as string | readonly string[] | string[]
+    if (_.isArray(value)) {
+      expectTypeOf(value).toEqualTypeOf<readonly string[] | string[]>()
+    } else {
+      expectTypeOf(value).toEqualTypeOf<string>()
+    }
+  })
+  test('value is string or Map', () => {
+    const value = {} as string | string[]
+    if (_.isArray(value)) {
+      expectTypeOf(value).toEqualTypeOf<string[]>()
+    } else {
+      expectTypeOf(value).toEqualTypeOf<string>()
+    }
+  })
+})
