6f0edc77baa11ad35122073c9a8c6f1a617d22fc
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-03 18:36:48 -0400

    refactor: use `isTagged` in `isObject`
    According to the docs, this function should return true for Object subclass instances, which it was not doing.

--- tests/typed/isObject.test.ts
+++ tests/typed/isObject.test.ts
@@ -1,6 +1,19 @@
 import * as _ from 'radashi'
 
 describe('isObject', () => {
+  test('returns true for object literal', () => {
+    const result = _.isObject({})
+    expect(result).toBeTruthy()
+  })
+  test('returns true for Object.create(null)', () => {
+    const result = _.isObject(Object.create(null))
+    expect(result).toBeTruthy()
+  })
+  test('returns true for class instance', () => {
+    class Data {}
+    const result = _.isObject(new Data())
+    expect(result).toBeTruthy()
+  })
   test('returns false for null', () => {
     const result = _.isObject(null)
     expect(result).toBeFalsy()
@@ -11,11 +24,6 @@ describe('isObject', () => {
   })
   test('returns false for boolean', () => {
     const result = _.isObject(false)
-    expect(result).toBeFalsy()
-  })
-  test('returns false for class instance', () => {
-    class Data {}
-    const result = _.isObject(new Data())
     expect(result).toBeFalsy()
   })
   test('returns false for number', () => {
@@ -29,9 +37,5 @@ describe('isObject', () => {
   test('returns false for array', () => {
     const result = _.isObject([1, 2, 3])
     expect(result).toBeFalsy()
-  })
-  test('returns true for object', () => {
-    const result = _.isObject({})
-    expect(result).toBeTruthy()
   })
 })
