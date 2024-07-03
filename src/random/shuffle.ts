84adbfcbead09d411c4bfc05f974a3ed9766d1f6
Euan MacKenzie <82283260+eumkz@users.noreply.github.com>
2024-07-03 23:29:36 +0000

    fix shuffle

--- src/random/shuffle.ts
+++ src/random/shuffle.ts
@@ -1,6 +1,19 @@
-export function shuffle<T>(array: readonly T[]): T[] {
-  return array
-    .map(a => ({ rand: Math.random(), value: a }))
-    .sort((a, b) => a.rand - b.rand)
-    .map(a => a.value)
+import * as _ from 'radashi'
+
+export function shuffle<T>(
+  array: readonly T[],
+  random: (min: number, max: number) => number = _.random,
+): T[] {
+  const newArray = array.slice()
+  for (
+    let idx = 0, randomIdx: number, item: T;
+    idx < array.length;
+    idx++
+  ) {
+    randomIdx = random(0, array.length - 1)
+    item = newArray[randomIdx]
+    newArray[randomIdx] = newArray[idx]
+    newArray[idx] = item
+  }
+  return newArray
 }
