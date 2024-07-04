a0a7605c16a26a99a469d1370cbd594e221c0ecb
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-04 02:52:56 -0400

    feat(clone): support cloning of Map/Set/Array objects

--- src/object/clone.ts
+++ src/object/clone.ts
@@ -1,30 +1,58 @@
-import { isPrimitive } from 'radashi'
+import { isArray, isFunction, isMap, isObject, isSet } from 'radashi'
+
+// biome-ignore lint/complexity/noBannedTypes:
+type UnsafeFunction = Function
 
 /**
- * Creates a shallow copy of the given obejct/value.
- * @param {*} obj value to clone @returns {*} shallow clone of the
- * given value
+ * Creates a shallow copy of the given object/value.
+ *
+ * It's assumed that you don't care about cloning `RegExp`/`Date`
+ * objects or typed arrays like `Uint8Array`. In fact, the only cloned
+ * object types are as follows:
+ * * plain objects (including `Object.create(null)` objects)
+ * * class instances
+ * * arrays
+ * * maps (but not `WeakMap`)
+ * * sets (but not `WeakSet`)
+ * * functions (but defined properties are not copied over)
  */
-export function clone<T>(obj: T): T {
-  // Primitive values do not need cloning.
-  if (isPrimitive(obj)) {
-    return obj
+export function clone<T>(
+  obj: T,
+): T extends (...args: infer Args) => infer R
+  ? (...args: Args) => R
+  : T extends UnsafeFunction
+    ? UnsafeFunction
+    : T extends readonly (infer U)[]
+      ? U[]
+      : T
+
+export function clone(obj: unknown): unknown {
+  if (isObject(obj)) {
+    const proto = Object.getPrototypeOf(obj)
+    const newObj =
+      typeof proto?.constructor === 'function'
+        ? new proto.constructor()
+        : Object.create(proto)
+
+    for (const key of Object.getOwnPropertyNames(obj)) {
+      newObj[key] = obj[key as keyof object]
+    }
+
+    return newObj
+  }
+
+  if (isArray(obj)) {
+    return [...obj]
+  }
+
+  if (isMap(obj) || isSet(obj)) {
+    return new (obj.constructor as any)(obj)
   }
 
   // Binding a function to an empty object creates a copy function.
-  if (typeof obj === 'function') {
+  if (isFunction(obj)) {
     return obj.bind({})
   }
 
-  const proto = Object.getPrototypeOf(obj)
-  const newObj =
-    typeof proto?.constructor === 'function'
-      ? new proto.constructor()
-      : Object.create(proto)
-
-  for (const key of Object.getOwnPropertyNames(obj)) {
-    newObj[key] = obj[key as keyof T]
-  }
-
-  return newObj
+  return obj
 }
