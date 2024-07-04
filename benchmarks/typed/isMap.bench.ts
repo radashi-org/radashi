2385946cb1e19cd63cfc3477e52f89b0194d2ae9
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-03 20:02:15 -0400

    chore: add benchmarks

--- benchmarks/typed/isMap.bench.ts
+++ benchmarks/typed/isMap.bench.ts
@@ -2,8 +2,7 @@ import * as _ from 'radashi'
 import { bench } from 'vitest'
 
 describe('isMap', () => {
-  bench('with no arguments', () => {
-    _.isMap()
+  bench('with valid input', () => {
+    _.isMap(new Map())
   })
 })
-
