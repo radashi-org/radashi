3df170c7da86eee71b204725c3edfa4672d017bd
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-07 19:04:51 -0400

    stop using reduceKeys
    This saves us 77 bytes.

--- src/object/pick.ts
+++ src/object/pick.ts
@@ -1,20 +1,30 @@
-import { type FilteredKeys, type KeyFilter, reduceKeys } from "radashi";
+import { type FilteredKeys, filterKey, isArray, type KeyFilter } from 'radashi'
 
 /**
  * Pick a list of properties from an object into a new object
  */
 export function pick<T extends object, F extends KeyFilter<T, keyof T>>(
-	obj: T,
-	filter: F,
-): Pick<T, FilteredKeys<T, F>> {
-	if (!obj) return {} as any;
-	return reduceKeys(
-		obj,
-		filter,
-		(acc, value, key) => {
-			acc[key] = value;
-			return acc;
-		},
-		{} as any,
-	);
+  obj: T,
+  filter: F,
+): Pick<T, FilteredKeys<T, F>>
+
+export function pick<T extends object>(
+  obj: T,
+  filter: KeyFilter<T, keyof T> | null,
+) {
+  if (!obj) {
+    return {}
+  }
+  let keys: (keyof T)[] = filter as any
+  if (isArray(filter)) {
+    filter = null
+  } else {
+    keys = Reflect.ownKeys(obj) as (keyof T)[]
+  }
+  return keys.reduce((acc, key) => {
+    if (filterKey(obj, key, filter)) {
+      acc[key] = obj[key]
+    }
+    return acc
+  }, {} as T)
 }
