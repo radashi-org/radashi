f2a064070fe53841ccd6b5a57a6c33de76d71b1d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-07 16:11:05 -0400

    feat: let `group` accept any iterable
    +121 bytes

--- src/array/group.ts
+++ src/array/group.ts
@@ -1,3 +1,5 @@
+import { reduceIterable, type ToIterableItem } from 'radashi'
+
 /**
  * Sorts an `array` of items into groups. The return value is a map
  * where the keys are the group IDs the given `getGroupId` function
@@ -10,19 +12,18 @@
  * // { even: [2], odd: [1, 3, 4] }
  * ```
  */
-export function group<T, Key extends string | number | symbol>(
-  array: readonly T[],
-  getGroupId: (item: T) => Key,
-): { [K in Key]?: T[] } {
-  return array.reduce(
+export function group<T extends object, Key extends string | number | symbol>(
+  iterable: T,
+  getGroupId: (item: ToIterableItem<T>) => Key,
+): { [K in Key]?: ToIterableItem<T>[] } {
+  return reduceIterable(
+    iterable,
     (acc, item) => {
       const groupId = getGroupId(item)
-      if (!acc[groupId]) {
-        acc[groupId] = []
-      }
+      acc[groupId] ??= []
       acc[groupId].push(item)
       return acc
     },
-    {} as Record<Key, T[]>,
+    {} as Record<Key, ToIterableItem<T>[]>,
   )
 }
