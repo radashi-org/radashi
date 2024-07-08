340124042ca838dbd5911a277a4759332cbcbc14
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-07 22:29:05 -0400

    feat: add `once` function (#80)

---
+++ src/curry/once.ts
@@ -0,0 +1,60 @@
+const onceSymbol: unique symbol = Symbol()
+
+/**
+ * The type of a function wrapped with `once`.
+ */
+export interface OnceFunction<
+  Args extends unknown[] = unknown[],
+  Return = unknown,
+  This = unknown,
+> {
+  (this: This, ...args: Args): Return
+  [onceSymbol]?: Return | typeof onceSymbol
+}
+
+/**
+ * Create a function that runs at most once, no matter how many times
+ * it's called. If it was already called before, returns the result
+ * from the first call. This is a lighter version of `memo()`.
+ *
+ * To allow the function to be called again, use `onceReset()`.
+ *
+ * ```ts
+ * const fn = once(() => Math.random())
+ * fn() // 0.5
+ * fn() // 0.5
+ * ```
+ */
+export const once: {
+  <Args extends unknown[], Return, This = unknown>(
+    fn: (this: This, ...args: Args) => Return,
+  ): (this: This, ...args: Args) => Return
+
+  /**
+   * Reset the result of a function that was created with `once`,
+   * allowing it to be called again.
+   *
+   * ```ts
+   * const fn = once(() => Math.random())
+   * fn() // 0.5
+   * fn() // 0.5
+   * once.reset(fn)
+   * fn() // 0.3
+   * fn() // 0.3
+   * ```
+   */
+  reset(fn: OnceFunction): void
+} = fn => {
+  const onceFn = function (...args: any) {
+    if (onceFn[onceSymbol] === onceSymbol) {
+      onceFn[onceSymbol] = fn.apply(this as any, args)
+    }
+    return onceFn[onceSymbol]
+  } as OnceFunction
+  onceFn[onceSymbol] = onceSymbol
+  return onceFn as typeof fn
+}
+
+once.reset = (fn: OnceFunction): void => {
+  fn[onceSymbol] = onceSymbol
+}
---
+++ tests/curry/once.test.ts
@@ -0,0 +1,39 @@
+import * as _ from 'radashi'
+
+describe('once', () => {
+  test('the function is only invoked once', () => {
+    let count = 0
+    const fn = _.once(() => ++count)
+
+    expect(fn()).toBe(1)
+    expect(fn()).toBe(1)
+    expect(fn()).toBe(1)
+  })
+  test('functions that return undefined are handled', () => {
+    let count = 0
+    const fn = _.once(() => {
+      count++
+    })
+
+    expect(fn()).toBeUndefined()
+    expect(fn()).toBeUndefined()
+    expect(fn()).toBeUndefined()
+    expect(count).toBe(1)
+  })
+  test('this is forwarded', () => {
+    const fn = _.once(function () {
+      return this
+    })
+    expect(fn.call(Math)).toBe(Math)
+  })
+})
+
+describe('onceReset', () => {
+  test('the result of the function is reset', () => {
+    let count = 0
+    const fn = _.once(() => count++)
+    const firstResult = fn()
+    _.once.reset(fn)
+    expect(fn()).not.toBe(firstResult)
+  })
+})
