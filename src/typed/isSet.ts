33d008d477cb9c81012e80f72f95af0ce7d4e67c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-07 23:23:43 -0400

    chore(types): use `unknown` instead of any

--- src/typed/isSet.ts
+++ src/typed/isSet.ts
@@ -3,9 +3,9 @@ import { type ExtractNotAny, isTagged } from 'radashi'
 export function isSet<Input>(
   value: Input,
 ): value is ReadonlySet<any> extends ExtractNotAny<Input, ReadonlySet<any>>
-  ? Extract<Input, ReadonlySet<any>>
+  ? Extract<Input, ReadonlySet<unknown>>
   : Set<any> extends ExtractNotAny<Input, Set<any>>
-    ? Extract<Input, Set<any>>
+    ? Extract<Input, Set<unknown>>
     : Set<unknown> extends Input
       ? Set<unknown>
       : never
