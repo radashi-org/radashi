14c93f34191fbfe44c28866ad58fafdaca54e541
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-07 23:20:08 -0400

    chore: add description to `isTagged`

--- src/typed/isTagged.ts
+++ src/typed/isTagged.ts
@@ -1,4 +1,12 @@
-/** @internal */
+/**
+ * Compare the given tag to the result of `Object.prototype.toString`.
+ *
+ * ⚠️ You probably won't use this except when implementing another type guard.
+ *
+ * ```ts
+ * isTagged('foo', '[object String]') // true
+ * ```
+ */
 export function isTagged(value: unknown, tag: string): boolean {
   return Object.prototype.toString.call(value) === tag
 }
