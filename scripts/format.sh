72245a3954dfc8e932a95d4fedb83ff9d552ebd1
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-08 18:28:50 -0400

    chore: allow filtering the `pnpm format` command
    Pass file names or a folder (like `docs` or `src`) to `pnpm format [...files]` and only matching files will be checked for formatting.

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,9 +3,42 @@ set -e
 # 1. Remember if the user has uncommitted changes (ignoring untracked files).
 UNCOMMITTED_CHANGES=$(git status --porcelain -uno)
 
+BIOME_GLOBS="src/**/* tests/**/* benchmarks/**/*"
+BIOME_FILES=$(pnpm glob $BIOME_GLOBS)
+
+PRETTIER_GLOBS="package.json README.md docs/**/* scripts/**/*"
+PRETTIER_FILES=$(pnpm glob $PRETTIER_GLOBS)
+
+# If arguments were passed, filter the list of files to only include those.
+if [ $# -gt 0 ]; then
+  FILTER=$(printf "|%s" "$@")
+  FILTER=${FILTER:1} # Remove the leading '|'
+
+  FILTERED_BIOME_FILES=""
+  for file in $BIOME_FILES; do
+    if echo "$file" | grep -qE "^($FILTER.*)"; then
+      FILTERED_BIOME_FILES="$FILTERED_BIOME_FILES $file"
+    fi
+  done
+  BIOME_FILES=$FILTERED_BIOME_FILES
+
+  FILTERED_PRETTIER_FILES=""
+  for file in $PRETTIER_FILES; do
+    if echo "$file" | grep -qE "^($FILTER.*)"; then
+      FILTERED_PRETTIER_FILES="$FILTERED_PRETTIER_FILES $file"
+    fi
+  done
+  PRETTIER_FILES=$FILTERED_PRETTIER_FILES
+fi
+
 # 2. Update the formatting.
-pnpm biome check --fix --unsafe
-pnpm prettier --write README.md docs/**/* scripts/**/*
+if [ -n "$BIOME_FILES" ]; then
+  pnpm biome check --diagnostic-level info --fix --unsafe $BIOME_FILES
+fi
+
+if [ -n "$PRETTIER_FILES" ]; then
+  pnpm prettier --write $PRETTIER_FILES
+fi
 
 # 3. Commit if there were no uncommitted changes, but there are now.
 if [ -z "$UNCOMMITTED_CHANGES" ] && [ -n "$(git status --porcelain -uno)" ]; then
