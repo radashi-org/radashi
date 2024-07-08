5ca291e04367556778a17854c389523bf9f67e2b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-07 23:31:14 -0400

    chore: add description to `isObject`

--- src/typed/isObject.ts
+++ src/typed/isObject.ts
@@ -1,5 +1,26 @@
 import { isTagged } from 'radashi'
 
+/**
+ * Returns true if `value` is a plain object, a class instance
+ * (excluding built-in classes like Date/RegExp), or an
+ * `Object.create(null)` result. Objects from [other realms][1] are
+ * also supported.
+ *
+ * [1]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof#instanceof_and_multiple_realms
+ *
+ * ```
+ * isObject({}) // true
+ * isObject(new Object()) // true
+ * isObject(Object.create(null)) // true
+ * isObject(new class {}) // true
+ *
+ * isObject([]) // false
+ * isObject(/.+/g) // false
+ * isObject(new Date()) // false
+ * isObject(new Map()) // false
+ * isObject(new Set()) // false
+ * ```
+ */
 export function isObject(value: unknown): value is object {
   return isTagged(value, '[object Object]')
 }
