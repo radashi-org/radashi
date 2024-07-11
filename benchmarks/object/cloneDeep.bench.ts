ccc4836a89677a14cb9ba7580ff549e721a9d4b4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-10 20:57:25 -0400

    add benchmark

--- benchmarks/object/cloneDeep.bench.ts
+++ benchmarks/object/cloneDeep.bench.ts
@@ -2,8 +2,13 @@ import * as _ from 'radashi'
 import { bench } from 'vitest'
 
 describe('cloneDeep', () => {
-  bench('with no arguments', () => {
-    _.cloneDeep()
+  const objects: any = _.list(0, 5, i => {
+    const object: any = {}
+    _.set(object, 'a.b.c.d.e.f.g.h.i.k.l.m.n.o.p', i)
+    return object
+  })
+
+  bench('dozens of nested plain objects', () => {
+    _.cloneDeep(objects)
   })
 })
-
