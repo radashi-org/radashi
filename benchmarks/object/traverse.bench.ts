300980549c1f23061c037cd9e6a45662555e3ba3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-11 16:26:06 -0400

    add benchmark

--- benchmarks/object/traverse.bench.ts
+++ benchmarks/object/traverse.bench.ts
@@ -2,8 +2,10 @@ import * as _ from 'radashi'
 import { bench } from 'vitest'
 
 describe('traverse', () => {
-  bench('with no arguments', () => {
-    _.traverse()
+  const root = {
+    a: { b: { c: { d: { e: 1 } } } },
+  }
+  bench('basic traversal', () => {
+    _.traverse(root, () => {})
   })
 })
-
