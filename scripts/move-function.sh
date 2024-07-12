15a407279edef6a5b57cd6d14154eab047b610ae
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-12 16:07:00 -0400

    chore: add move-function script

---
+++ scripts/move-function.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+IFS="/" read -r GROUP FUNC <<< "$1"
+DEST="$2"
+
+mkdir -p "src/$DEST"
+git mv "src/$GROUP/$FUNC.ts" "src/$DEST/$FUNC.ts"
+
+mkdir -p "docs/$DEST"
+git mv "docs/$GROUP/$FUNC.mdx" "docs/$DEST/$FUNC.mdx"
+
+mkdir -p "benchmarks/$DEST"
+git mv "benchmarks/$GROUP/$FUNC.bench.ts" "benchmarks/$DEST/$FUNC.bench.ts"
+
+mkdir -p "tests/$DEST"
+git mv "tests/$GROUP/$FUNC.test.ts" "tests/$DEST/$FUNC.test.ts"
