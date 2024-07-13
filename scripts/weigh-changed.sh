31fd8a1c36c1614d243f2d4ae7ce6c3c79980d34
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-12 23:53:42 -0400

    Revert "chore: debug weigh-changed.sh"
    This reverts commit 5d016f662594936a73bc694cb7c2d74cdb0ee9c9.

--- scripts/weigh-changed.sh
+++ scripts/weigh-changed.sh
@@ -44,7 +44,7 @@ PREV_SIZES=()
 
 # Collect previous sizes if there are no uncommitted changes or it's CI.
 if [ -n "$CI" ] || [ -z "$(git status -s)" ]; then
-  git checkout "$TARGET_BRANCH"
+  git checkout "$TARGET_BRANCH" &> /dev/null
 
   i=0
   for file in "${FILE_NAMES[@]}"; do
