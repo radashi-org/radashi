d15933c9fe87084e8452a2baae6d4b9d0998fcdf
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-13 15:56:41 -0400

    rename to `castMapping`

---
+++ src/casted/castMapping.ts
@@ -0,0 +1,86 @@
+import { type Any, type CompatibleProperty, isFunction } from 'radashi'
+
+/**
+ * Cast the `mapping` value into a mapping function.
+ *
+ * - If `mapping` is a function, it's returned as is.
+ * - If `mapping` is a property name, the mapping uses it to retrieve
+ *   the property value from a given object.
+ * - If `mapping` is nullish, the mapping is `(input: T) => input`.
+ *
+ * @see https://radashi-org.github.io/reference/casted/castMapping
+ * @example
+ * ```ts
+ * const getName = castMapping('name')
+ * const getFullName = castMapping(person => {
+ *   return `${person.firstName} ${person.lastName}`
+ * })
+ *
+ * getName({ name: 'John' }) // => 'John'
+ * getFullName({ firstName: 'John', lastName: 'Doe' }) // => 'John Doe'
+ * ```
+ */
+export function castMapping<TMapping extends Mapping | null | undefined>(
+  mapping: TMapping,
+): MappingFunction<TMapping> {
+  return isFunction(mapping)
+    ? mapping
+    : mapping != null
+      ? <TInput extends MappedInput<TMapping>>(input: TInput) =>
+          input[mapping as keyof TInput] as MappedOutput<TMapping>
+      : input => input as MappedOutput<TMapping>
+}
+
+/**
+ * A value that can be casted with `castMapping`.
+ *
+ * @see https://radashi-org.github.io/reference/casted/castMapping
+ */
+export type Mapping<T = any, U = any> =
+  | ((arg: T) => U)
+  | CompatibleProperty<T, U>
+
+/**
+ * The input type of a mapping function created with `castMapping`.
+ *
+ * @see https://radashi-org.github.io/reference/casted/castMapping
+ */
+export type MappedInput<TMapping> = TMapping extends (arg: infer Arg) => any
+  ? [Arg] extends [Any]
+    ? unknown
+    : Arg
+  : TMapping extends keyof any
+    ?
+        | { [P in TMapping]: any }
+        | (TMapping extends number ? readonly any[] : never)
+    : unknown
+
+/**
+ * The return type of a mapping function created with `castMapping`.
+ *
+ * @see https://radashi-org.github.io/reference/casted/castMapping
+ */
+export type MappedOutput<TMapping, TInput = any> = TMapping extends (
+  data: TInput,
+) => infer Result
+  ? [Result] extends [Any]
+    ? unknown
+    : Result
+  : [TInput] extends [Any]
+    ? unknown
+    : TMapping extends null | undefined
+      ? TInput
+      : TMapping extends keyof TInput
+        ? TInput[TMapping]
+        : never
+
+/**
+ * A mapping function created with `castMapping`.
+ *
+ * @see https://radashi-org.github.io/reference/casted/castMapping
+ */
+export type MappingFunction<TMapping extends Mapping | null | undefined> = <
+  TInput extends MappedInput<TMapping>,
+>(
+  input: TInput,
+) => MappedOutput<TMapping, TInput>
--- tests/casted/castMapping.test.ts
+++ tests/casted/castMapping.test.ts
@@ -1,30 +1,33 @@
 import * as _ from 'radashi'
 
-describe('getter', () => {
+describe('castMapping', () => {
   test('returns the same function if provided a function', () => {
     const fn = (arg: number) => arg * 2
-    const get = _.getter(fn)
+    const get = _.castMapping(fn)
     expect(get(5)).toBe(10)
   })
 
   test('returns the property value if provided a property name', () => {
     const obj = { a: 1, b: 2 }
-    expect(_.getter('a')(obj)).toBe(1)
+    const get = _.castMapping('a')
+    expect(get(obj)).toBe(1)
   })
 
   test('returns the original argument if provided undefined', () => {
     const arg = { a: 1 }
-    expect(_.getter(undefined)(arg)).toBe(arg)
+    const get = _.castMapping(undefined)
+    expect(get(arg)).toBe(arg)
   })
 
   test('returns never for invalid property access', () => {
     const obj = { a: 1 }
+    const get = _.castMapping('invalid')
     // @ts-expect-error
-    expect(_.getter('invalid')(obj)).toBe(undefined)
+    expect(get(obj)).toBe(undefined)
   })
 
   test('handles arrays correctly', () => {
     const arr = [1, 2, 3]
-    expect(_.getter(0)(arr)).toBe(1)
+    expect(_.castMapping(0)(arr)).toBe(1)
   })
 })
