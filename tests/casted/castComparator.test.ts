f44479ba8911caff20ca49d40e83f36d6b086c2d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-13 19:40:00 -0400

    add typing tests

--- tests/casted/castComparator.test.ts
+++ tests/casted/castComparator.test.ts
@@ -1,6 +1,6 @@
 import * as _ from 'radashi'
 
-describe('compare function', () => {
+describe('castComparator', () => {
   test('accepts a property name', () => {
     const compare = _.castComparator('a')
     expect(compare({ a: 1 }, { a: 2 })).toBe(-1)
@@ -8,18 +8,18 @@ describe('compare function', () => {
     expect(compare({ a: 1 }, { a: 1 })).toBe(0)
   })
   test('accepts a property name and a compare function', () => {
-    const order = vi.fn((a: number, b: number) => a - b)
-    const compare = _.castComparator('a', order)
+    const ascending = vi.fn((a: number, b: number) => a - b)
+    const compare = _.castComparator('a', ascending)
     expect(compare({ a: 1 }, { a: 2 })).toBe(-1)
     expect(compare({ a: 2 }, { a: 1 })).toBe(1)
-    expect(order).toHaveBeenCalledTimes(2)
+    expect(ascending).toHaveBeenCalledTimes(2)
   })
-  test('accepts a getter', () => {
+  test('accepts a mapping function', () => {
     const compare = _.castComparator((obj: { a: number }) => obj.a)
     expect(compare({ a: 1 }, { a: 2 })).toBe(-1)
     expect(compare({ a: 2 }, { a: 1 })).toBe(1)
   })
-  test('accepts a getter and a compare function', () => {
+  test('accepts a mapping function and a compare function', () => {
     const order = vi.fn((a: number, b: number) => a - b)
     const compare = _.castComparator((obj: { a: number }) => obj.a, order)
     expect(compare({ a: 1 }, { a: 2 })).toBe(-1)
