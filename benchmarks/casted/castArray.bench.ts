9f9c804b1dd0ef079a1ecef8cd2c53b38da2665f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-13 17:30:40 -0400

    add benchmarks

---
+++ benchmarks/casted/castArray.bench.ts
@@ -0,0 +1,14 @@
+import * as _ from 'radashi'
+import { bench } from 'vitest'
+
+describe('castArray', () => {
+  bench('with an array', () => {
+    _.castArray(new Array(100))
+  })
+  bench('with number', () => {
+    _.castArray(1)
+  })
+  bench('with null', () => {
+    _.castArray(null)
+  })
+})
