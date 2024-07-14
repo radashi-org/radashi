eea8ea76f376da488ea4349110ab6db185b18780
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-13 22:39:27 -0400

    chore: add example to `partob`

--- src/curry/partob.ts
+++ src/curry/partob.ts
@@ -4,6 +4,19 @@
  *
  * @see https://radashi-org.github.io/reference/curry/partob
  * @example
+ * ```ts
+ * const add = (
+ *   {a = 0, b = 0, c = 0}: {
+ *     a?: number,
+ *     b?: number,
+ *     c?: number
+ *   }
+ * ) => a + b + c
+ *
+ * const addPartial = partob(add, { a: 1 })
+ * addPartial({ b: 2 }) // 3
+ * addPartial({ b: 1, c: 5 }) // 7
+ * ```
  */
 export function partob<T, K, PartialArgs extends Partial<T>>(
   fn: (args: T) => K,
