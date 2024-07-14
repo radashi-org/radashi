f010ad47d70d1612668350441d78c3e981ac6cff
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-13 22:18:29 -0400

    feat(mapify): provide an index argument to the callbacks (#100)

--- tests/array/mapify.test.ts
+++ tests/array/mapify.test.ts
@@ -33,4 +33,12 @@ describe('mapify', () => {
     expect(result.size).toBe(1)
     expect(result.get('a')?.word).toBe('hello')
   })
+  test('an index is passed to getKey and getValue', () => {
+    const result = _.mapify(
+      list,
+      (x, i) => x.id + i,
+      (x, i) => x.word + i,
+    )
+    expect(result.get('a0')).toBe('hello0')
+  })
 })
