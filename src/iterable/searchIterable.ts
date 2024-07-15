015ab396bebccca93db191bb45f1086d3bcd6242
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-15 15:50:01 -0400

    improve searchIterable

--- src/iterable/searchIterable.ts
+++ src/iterable/searchIterable.ts
@@ -1,13 +1,26 @@
-import { toIterable, type ToIterableItem } from 'radashi'
+import { castIterable, isArray, type CastIterableItem } from 'radashi'
 
 export function searchIterable<T extends object>(
   iterable: T,
-  match: (item: ToIterableItem<T>, index: number) => boolean,
-): ToIterableItem<T> | undefined {
-  let index = 0
-  for (const item of toIterable(iterable)) {
-    if (match(item, index++)) {
-      return item
+  match: (item: CastIterableItem<T>, index: number) => boolean,
+): CastIterableItem<T> | undefined {
+  let item: CastIterableItem<T> | undefined
+  if (isArray(iterable)) {
+    // Note: We can't use Array.prototype.find here, because it
+    // doesn't respect the sparseness of an array.
+    ;(iterable as any[]).some((it, i) => {
+      if (match(it, i)) {
+        item = it
+        return true
+      }
+    })
+  } else {
+    let index = 0
+    for (item of castIterable(iterable)) {
+      if (match(item, index++)) {
+        break
+      }
     }
   }
+  return item
 }
