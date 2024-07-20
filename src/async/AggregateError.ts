5668b85246219776bf9b98fb8734f528521b0ed9
Marlon Passos <1marlonpassos@gmail.com>
2024-07-20 13:58:30 -0300

    feat: use native AggregateError if available (#116)
    Co-authored-by: Alec Larson <1925840+aleclarson@users.noreply.github.com>

--- src/async/AggregateError.ts
+++ src/async/AggregateError.ts
@@ -1,16 +1,31 @@
+/// <reference lib="es2021.promise" />
+
 /**
- * Support for the built-in AggregateError is still new. Node < 15
- * doesn't have it so patching here.
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AggregateError#browser_compatibility
+ * The `AggregateError` object represents an error when several errors
+ * need to be wrapped in a single error.
+ *
+ * As this error type is relatively new, it's not available in every
+ * environment supported by Radashi (last checked on July 20, 2024).
+ * When it's not globally defined, Radashi provides a polyfill.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AggregateError
  */
-export class AggregateError extends Error {
-  errors: Error[]
-  constructor(errors: Error[] = []) {
-    super()
-    const name = errors.find(e => e.name)?.name ?? ''
-    this.name = `AggregateError(${name}...)`
-    this.message = `AggregateError with ${errors.length} errors`
-    this.stack = errors.find(e => e.stack)?.stack ?? this.stack
-    this.errors = errors
-  }
-}
+const AggregateErrorOrPolyfill: AggregateErrorConstructor =
+  // eslint-disable-next-line compat/compat
+  globalThis.AggregateError ??
+  (class AggregateError extends Error {
+    errors: Error[]
+    constructor(errors: Error[] = []) {
+      super()
+      const name = errors.find(e => e.name)?.name ?? ''
+      this.name = `AggregateError(${name}...)`
+      this.message = `AggregateError with ${errors.length} errors`
+      this.stack = errors.find(e => e.stack)?.stack ?? this.stack
+      this.errors = errors
+    }
+  } as unknown as AggregateErrorConstructor)
+
+// Do not export directly, so the polyfill isn't renamed to
+// `AggregateError2` at build time (which ESBuild does to prevent
+// variable shadowing).
+export { AggregateErrorOrPolyfill as AggregateError }
