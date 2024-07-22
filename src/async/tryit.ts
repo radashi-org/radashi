3c0797c23799f974a1693754186beb10a636132b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-22 18:13:26 -0400

    refactor: use `toResult` inside `tryit`

--- src/async/tryit.ts
+++ src/async/tryit.ts
@@ -1,47 +1,21 @@
-import { isPromise } from 'radashi'
-
-/**
- * The result of a `tryit` function.
- *
- * If the function returns a promise, the result is a promise that
- * resolves to an error-first callback-_like_ array response as
- * `[Error, result]`.
- *
- * If the function returns a non-promise, the result is an error-first
- * callback-_like_ array response as `[Error, result]`.
- *
- * @see https://radashi-org.github.io/reference/async/tryit
- * @example
- * ```ts
- * const [err, result] = await tryit(async () => {
- *   return await fetch('https://example.com')
- * })
- * ```
- */
-export type TryitResult<Return> = Return extends Promise<any>
-  ? Promise<[Error, undefined] | [undefined, Awaited<Return>]>
-  : [Error, undefined] | [undefined, Return]
+import { toResult, type ToResult } from 'radashi'
 
 /**
  * A helper to try an async function without forking the control flow.
  * Returns an error-first callback-_like_ array response as `[Error,
  * result]`
  */
-export function tryit<Args extends any[], Return>(
-  func: (...args: Args) => Return,
-): (...args: Args) => TryitResult<Return> {
+export function tryit<TArgs extends any[], TReturn, TError = Error>(
+  func: (...args: TArgs) => TReturn,
+): (...args: TArgs) => ToResult<TReturn, TError> {
   return (...args) => {
+    let result: TReturn
     try {
-      const result = func(...args)
-      if (isPromise(result)) {
-        return result
-          .then(value => [undefined, value])
-          .catch(err => [err, undefined]) as TryitResult<Return>
-      }
-      return [undefined, result] as TryitResult<Return>
+      result = func(...args)
     } catch (err) {
-      return [err, undefined] as TryitResult<Return>
+      return [err, undefined] as ToResult<TReturn, TError>
     }
+    return toResult(result)
   }
 }
 
