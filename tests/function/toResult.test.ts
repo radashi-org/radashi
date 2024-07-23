5a4e33180304d99de78129633741ae2b34f10c6a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-23 12:44:47 -0400

    update toResult tests

--- tests/function/toResult.test.ts
+++ tests/function/toResult.test.ts
@@ -1,15 +1,17 @@
 import * as _ from 'radashi'
 
 describe('_.toResult', () => {
-  test('should return a Result tuple for non-promise values', () => {
+  test('primitive as input', () => {
     const result = _.toResult(null, 'hello')
     expect(result).toEqual([undefined, 'hello'])
-
-    const result2 = _.toResult(new TypeError('hello'))
-    expect(result2).toEqual([new TypeError('hello'), undefined])
   })
 
-  test('should return a ResultPromise for promise values', async () => {
+  test('error as input', () => {
+    const result = _.toResult(new TypeError('hello'))
+    expect(result).toEqual([new TypeError('hello'), undefined])
+  })
+
+  test('resolved promise as input', async () => {
     const result = _.toResult(null, Promise.resolve('hello'))
     expect(result).toBeInstanceOf(Promise)
     await expect(result).resolves.toEqual([undefined, 'hello'])
@@ -23,7 +25,7 @@ describe('_.toResult', () => {
     await expect(result3).resolves.toEqual([new TypeError('hello'), undefined])
   })
 
-  test('should handle rejected promises correctly', async () => {
+  test('rejected promise as input', async () => {
     const result = _.toResult(Promise.reject(new TypeError('error')))
     await expect(result).resolves.toEqual([new TypeError('error'), undefined])
 
@@ -31,19 +33,40 @@ describe('_.toResult', () => {
     await expect(result2).resolves.toEqual([new TypeError('error'), undefined])
   })
 
-  test('should handle Result objects correctly', async () => {
+  test('Result tuple as input', () => {
+    // Result tuple as 2nd argument is wrapped in another Result tuple.
+    const result = _.toResult(null, [undefined, 'hello'])
+    expect(result).toEqual([undefined, [undefined, 'hello']])
+  })
+
+  test('Result promise as input', async () => {
     // This call receives a Result tuple as its 1st argument, which is
     // exactly what the returned promise will resolve with.
-    const result = _.toResult(Promise.resolve([undefined, 'hello']))
+    const promise = Promise.resolve([undefined, 'hello'])
+    const result = _.toResult(promise)
+    expect(result).not.toBe(promise)
     await expect(result).resolves.toEqual([undefined, 'hello'])
 
     // This call receives a promise for a Result tuple as its 2nd argument, which is
     // exactly what the returned promise will resolve with.
-    const result2 = _.toResult(null, Promise.resolve([new Error(), undefined]))
+    const promise2 = Promise.resolve([new Error(), undefined])
+    const result2 = _.toResult(null, promise2)
+    expect(result2).not.toBe(promise2)
     await expect(result2).resolves.toEqual([new Error(), undefined])
 
     // Invalid tuple is not used as a Result tuple.
-    const result3 = _.toResult(null, Promise.resolve<number[]>([1, 1]))
+    const promise3 = Promise.resolve<number[]>([1, 1])
+    const result3 = _.toResult(null, promise3)
+    expect(result3).not.toBe(promise3)
     await expect(result3).resolves.toEqual([undefined, [1, 1]])
+  })
+
+  test('toResult promise as input', () => {
+    const promise1 = Promise.resolve('hello')
+    const promise2 = _.toResult(promise1)
+    const promise3 = _.toResult(promise2)
+
+    expect(promise1).not.toBe(promise2)
+    expect(promise2).toBe(promise3)
   })
 })
