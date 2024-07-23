23b4f9f87a00b70225c771cbfe1d19e476818bf7
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-07-23 13:08:09 -0400

    style: wrap tests in describe block

--- tests/curry/throttle.test.ts
+++ tests/curry/throttle.test.ts
@@ -36,48 +36,50 @@ describe('throttle', () => {
     assert.deepEqual(results, [false, true, true, true, false])
   })
 
-  test('single call with trailing option is set to `true` calls source function once', async () => {
-    let calls = 0
-    const func = _.throttle({ interval, trailing: true }, () => calls++)
-    func()
-    expect(calls).toBe(1)
-    vi.advanceTimersByTime(interval + 10)
-    expect(calls).toBe(1)
-  })
+  describe('trailing option', () => {
+    test('single call with trailing option is set to `true` calls source function once', async () => {
+      let calls = 0
+      const func = _.throttle({ interval, trailing: true }, () => calls++)
+      func()
+      expect(calls).toBe(1)
+      vi.advanceTimersByTime(interval + 10)
+      expect(calls).toBe(1)
+    })
 
-  test('repeated calls with trailing option is set to `true` calls source function again on trailing edge', async () => {
-    let calls = 0
-    const func = _.throttle({ interval, trailing: true }, () => calls++)
-    func()
-    expect(calls).toBe(1)
-    vi.advanceTimersByTime(10)
-    func()
-    vi.advanceTimersByTime(interval + 10)
-    expect(calls).toBe(2)
-  })
+    test('repeated calls with trailing option is set to `true` calls source function again on trailing edge', async () => {
+      let calls = 0
+      const func = _.throttle({ interval, trailing: true }, () => calls++)
+      func()
+      expect(calls).toBe(1)
+      vi.advanceTimersByTime(10)
+      func()
+      vi.advanceTimersByTime(interval + 10)
+      expect(calls).toBe(2)
+    })
 
-  test('with trailing option is set to `true`, throttling is still effective after a trailing invocation', async () => {
-    let calls = 0
-    const func = _.throttle({ interval, trailing: true }, () => calls++)
-    func()
-    func()
-    expect(calls).toBe(1)
-    vi.advanceTimersByTime(10)
-    func()
-    func()
-    expect(calls).toBe(1)
-    vi.advanceTimersByTime(interval)
-    // By now, the trailing call should have occurred
-    expect(calls).toBe(2)
+    test('with trailing option is set to `true`, throttling is still effective after a trailing invocation', async () => {
+      let calls = 0
+      const func = _.throttle({ interval, trailing: true }, () => calls++)
+      func()
+      func()
+      expect(calls).toBe(1)
+      vi.advanceTimersByTime(10)
+      func()
+      func()
+      expect(calls).toBe(1)
+      vi.advanceTimersByTime(interval)
+      // By now, the trailing call should have occurred
+      expect(calls).toBe(2)
 
-    vi.advanceTimersByTime(10)
-    func()
-    func()
-    // This call should still be throttled
-    expect(calls).toBe(2)
+      vi.advanceTimersByTime(10)
+      func()
+      func()
+      // This call should still be throttled
+      expect(calls).toBe(2)
 
-    vi.advanceTimersByTime(interval + 10)
-    // By now another trailing call should have happened
-    expect(calls).toBe(3)
+      vi.advanceTimersByTime(interval + 10)
+      // By now another trailing call should have happened
+      expect(calls).toBe(3)
+    })
   })
 })
