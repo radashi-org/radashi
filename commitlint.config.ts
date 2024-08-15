0f6bd16c4001efe6ac91dea76a7cd7a901c301dd
marlonpassos <marlon.passos@cadastra.com>
2024-08-15 03:51:11 +0000

    chore: add types about commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,11 @@
-export default {
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0],
     'footer-max-line-length': [0],
   },
 }
+
+export default Configuration;
