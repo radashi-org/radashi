41791488bb08a4b6451488d1ff66eba23b1cf36f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-15 17:50:19 -0400

    ci: fix PR checkout (take 3)

--- scripts/checkout-pr.sh
+++ scripts/checkout-pr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-set +x
+set -x
 
 # Fetch the PR contents
 git remote add pr $PR_REPO_URL
@@ -11,7 +11,7 @@ BASE_COMMIT=$(git merge-base HEAD "pr/$PR_HEAD_REF")
 git fetch origin $BASE_COMMIT
 
 # Import changes from the PR into the current branch without committing
-git cherry-pick -n "$BASE_COMMIT..$PR_HEAD_REF" -X theirs
+git cherry-pick -n $BASE_COMMIT.."$PR_HEAD_REF" -X theirs
 
 # List the affected files for debugging purposes
 git diff --name-status --staged
