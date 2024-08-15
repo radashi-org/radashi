bbe748f919e39c7d4828b9ce3a2c0333d0904eb5
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-15 17:47:32 -0400

    ci: fix PR checkout (take 2)
    Ensure a "bad revision" error does not happen by fetching the base commit from origin.

--- scripts/checkout-pr.sh
+++ scripts/checkout-pr.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -e
+set +x
 
 # Fetch the PR contents
 git remote add pr $PR_REPO_URL
 git fetch pr "$PR_HEAD_REF"
 
-# Import changes from the PR into the current branch without committing
+# Fetch the base commit
 BASE_COMMIT=$(git merge-base HEAD "pr/$PR_HEAD_REF")
+git fetch origin $BASE_COMMIT
+
+# Import changes from the PR into the current branch without committing
 git cherry-pick -n "$BASE_COMMIT..$PR_HEAD_REF" -X theirs
 
 # List the affected files for debugging purposes
