934632e4d5f785d76a94f9edb93a57d2e5574c29
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-15 18:02:48 -0400

    ci: remove `-X theirs` from cherry-pick in checkout-pr script
    Using a merge strategy only risks confusion when the merge leads to code being removed when it shouldn‘t be. Therefore, it‘s better to report a merge conflict and have the PR author resolve it manually.

--- scripts/checkout-pr.sh
+++ scripts/checkout-pr.sh
@@ -8,7 +8,7 @@ git fetch pr "$PR_HEAD_REF"
 
 # Import changes from the PR into the current branch without committing
 BASE_COMMIT=$(git merge-base HEAD "pr/$PR_HEAD_REF")
-git cherry-pick -n "$BASE_COMMIT..pr/$PR_HEAD_REF" -X theirs
+git cherry-pick -n "$BASE_COMMIT..pr/$PR_HEAD_REF"
 
 # List the affected files for debugging purposes
 git diff --name-status --staged
