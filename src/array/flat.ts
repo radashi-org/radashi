158659b392955eda866f3dc0190df90dad43f33a
marlonpassos <marlon.passos@cadastra.com>
2024-08-15 04:24:26 +0000

    revert: alert about depreciation
    This reverts commit ed5847155217c4dbddc7f5474ba734888da7b1e6.

--- src/array/flat.ts
+++ src/array/flat.ts
@@ -8,8 +8,6 @@
  * flat([[1, 2], [[3], 4], [5]])
  * // [1, 2, [3], 4, 5]
  * ```
- *
- * @deprecated - use [Array.prototype.flat](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat) insted
  */
 export function flat<T>(lists: readonly T[][]): T[] {
   return lists.reduce((acc, list) => {
