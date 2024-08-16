e507f46c7b4d72f60cd6da6ae46a4dc78de5475c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-15 21:41:38 -0400

    move @deprecated tag up a bit

--- src/array/flat.ts
+++ src/array/flat.ts
@@ -2,14 +2,14 @@
  * Given an array of arrays, returns a single dimensional array with
  * all items in it.
  *
+ * @deprecated Use [Array.prototype.flat](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat) instead.
+ *
  * @see https://radashi-org.github.io/reference/array/flat
  * @example
  * ```ts
  * flat([[1, 2], [[3], 4], [5]])
  * // [1, 2, [3], 4, 5]
  * ```
- *
- * @deprecated Use [Array.prototype.flat](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat) instead.
  */
 export function flat<T>(lists: readonly T[][]): T[] {
   return lists.flat()
