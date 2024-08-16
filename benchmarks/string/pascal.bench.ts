371f7dc068e9642a95d1a3f5e2b75797d138ea3c
marlonpassos <marlon.passos@cadastra.com>
2024-08-16 05:31:13 +0000

    perf(pascal): add more test cases

--- benchmarks/string/pascal.bench.ts
+++ benchmarks/string/pascal.bench.ts
@@ -5,4 +5,13 @@ describe('pascal', () => {
   bench('with valid input', () => {
     _.pascal('hello world')
   })
+  bench("wiht camelCase input", () => {
+    _.pascal("fooBar")
+  })
+  bench("with non alphanumerics", () => {
+    _.pascal("Exobase Starter_flash AND-go")
+  })
+  bench("With null input", () => {
+    _.pascal(null as any)
+  })
 })
