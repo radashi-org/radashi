e099186c06f052062d00c59dfca67e194dc053ac
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-16 17:53:32 -0400

    fix typos + remove one benchmark

--- benchmarks/string/pascal.bench.ts
+++ benchmarks/string/pascal.bench.ts
@@ -5,13 +5,10 @@ describe('pascal', () => {
   bench('with valid input', () => {
     _.pascal('hello world')
   })
-  bench('wiht camelCase input', () => {
+  bench('with camelCase input', () => {
     _.pascal('fooBar')
   })
   bench('with non alphanumerics', () => {
     _.pascal('Exobase Starter_flash AND-go')
-  })
-  bench('With null input', () => {
-    _.pascal(null as any)
   })
 })
