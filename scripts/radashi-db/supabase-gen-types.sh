fb02af64c92f8f7288fd4e73b6d655e2a00093ed
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-16 22:03:06 -0400

    chore: avoid installing `supabase` CLI unless updating the generated types

--- scripts/radashi-db/supabase-gen-types.sh
+++ scripts/radashi-db/supabase-gen-types.sh
@@ -3,4 +3,4 @@
 # Get the directory of this script
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-pnpm supabase gen types --lang=typescript --project-id=yucyhkpmrdbucitpovyj --schema=public > $SCRIPT_DIR/supabase.types.ts
+pnpm dlx supabase@1.187.10 gen types --lang=typescript --project-id=yucyhkpmrdbucitpovyj --schema=public > $SCRIPT_DIR/supabase.types.ts
