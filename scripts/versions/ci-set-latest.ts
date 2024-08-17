cf90fc52bed212a5195d5a541572c1f5e0551c18
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-17 19:20:30 -0400

    chore: refactor publish-beta workflow to TypeScript

--- scripts/versions/ci-set-latest.ts
+++ scripts/versions/ci-set-latest.ts
@@ -1,27 +1,11 @@
-import { supabase } from 'radashi-db/supabase.js'
+import { trackVersion } from './src/trackVersion'
 
 main()
 
 async function main() {
   const [version, ref] = process.argv.slice(2)
-  const channel = version.match(/-(\w+)/)?.[1] ?? 'stable'
-
-  const value = { version, ref }
-  console.log('Updating with:', value)
-
-  const { error: upsertError } = await supabase.from('meta').upsert([
-    {
-      id: 'latest_version',
-      value,
-    },
-    {
-      id: channel + '_version',
-      value,
-    },
-  ])
-
-  if (upsertError) {
-    console.error('Error upserting version data:', upsertError)
+  await trackVersion(version, ref).catch(error => {
+    console.error(error.message)
     process.exit(1)
-  }
+  })
 }
