2db7ed7fdf9f648779d7863fc5ef32000d419aaf
marlonpassos <marlon.passos@cadastra.com>
2024-08-20 22:01:09 +0000

    docs(first): add search terms and use cases

--- src/array/diff.ts
+++ src/array/diff.ts
@@ -13,26 +13,26 @@
  * ```
  */
 export function diff<T>(
-  root: readonly T[],
-  other: readonly T[],
-  identity: (item: T) => string | number | symbol = (t: T) =>
-    t as unknown as string | number | symbol,
+	root: readonly T[],
+	other: readonly T[],
+	identity: (item: T) => string | number | symbol = (t: T) =>
+		t as unknown as string | number | symbol,
 ): T[] {
-  if (!root?.length && !other?.length) {
-    return []
-  }
-  if (root?.length === undefined) {
-    return [...other]
-  }
-  if (!other?.length) {
-    return [...root]
-  }
-  const bKeys = other.reduce(
-    (acc, item) => {
-      acc[identity(item)] = true
-      return acc
-    },
-    {} as Record<string | number | symbol, boolean>,
-  )
-  return root.filter(a => !bKeys[identity(a)])
+	if (!root?.length && !other?.length) {
+		return [];
+	}
+	if (root?.length === undefined) {
+		return [...other];
+	}
+	if (!other?.length) {
+		return [...root];
+	}
+	const bKeys = other.reduce(
+		(acc, item) => {
+			acc[identity(item)] = true;
+			return acc;
+		},
+		{} as Record<string | number | symbol, boolean>,
+	);
+	return root.filter((a) => !bKeys[identity(a)]);
 }
