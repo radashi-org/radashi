7e883ee0528642cac8cd026892b3b1edeefe93db
Yam Borodetsky <hi@yam.codes>
2024-09-11 22:43:39 +0500

    docs: apply code review suggestions

--- src/array/product.ts
+++ src/array/product.ts
@@ -1,12 +1,20 @@
 /**
- * Perform a Cartesian product of arrays, combining all elements from the
- * input arrays into all possible combinations.
+ * Creates an [n-ary Cartesian product](https://en.wikipedia.org/wiki/Cartesian_product#n-ary_Cartesian_product) from the given arrays.
  *
  * @see https://radashi.js.org/reference/array/product
  * @example
  * ```ts
  * product([['red', 'blue'], ['big', 'small'], ['fast', 'slow']])
- * // => [['red', 'big', 'fast'], ['red', 'big', 'slow'], ['red', 'small', 'fast'], ['red', 'small', 'slow'], ['blue', 'big', 'fast'], ['blue', 'big', 'slow'], ['blue', 'small', 'fast'], ['blue', 'small', 'slow']]
+ * // [
+ * //   ['red', 'big', 'fast'],
+ * //   ['red', 'big', 'slow'],
+ * //   ['red', 'small', 'fast'],
+ * //   ['red', 'small', 'slow'],
+ * //   ['blue', 'big', 'fast'],
+ * //   ['blue', 'big', 'slow'],
+ * //   ['blue', 'small', 'fast'],
+ * //   ['blue', 'small', 'slow']
+ * // ]
  * ```
  */
 export function product<T>(arrays: T[][]): T[][] {
