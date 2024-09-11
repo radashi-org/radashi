b579e1ffe3f026ee3164921c9f3fc8483871dfc4
Yam Borodetsky <hi@yam.codes>
2024-09-12 00:03:46 +0500

    fix: rename to `cartesianProduct`
    + Add type tests
    + Update tests to support multiple types
    + Update doc to display multiple types

--- benchmarks/array/cartesianProduct.bench.ts
+++ benchmarks/array/cartesianProduct.bench.ts
@@ -1,27 +1,27 @@
 import * as _ from 'radashi'
 
-describe('product', () => {
+describe('cartesianProduct', () => {
   bench('with no arguments', () => {
-    _.product([])
+    _.cartesianProduct([])
   })
 
   bench('with single empty array', () => {
-    _.product([[]])
+    _.cartesianProduct([[]])
   })
 
   bench('with one non-empty array (n=1)', () => {
-    _.product([['a', 'b', 'c']])
+    _.cartesianProduct([['a', 'b', 'c']])
   })
 
   bench('with two small arrays (n=2)', () => {
-    _.product([
+    _.cartesianProduct([
       ['red', 'blue'],
       ['fast', 'slow'],
     ])
   })
 
   bench('with three small arrays (n=3)', () => {
-    _.product([
+    _.cartesianProduct([
       ['red', 'blue'],
       ['fast', 'slow'],
       ['big', 'small'],
