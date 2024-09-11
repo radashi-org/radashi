e04be349056f6af6eae0245d7521e3c2f8f18025
Yam Borodetsky <hi@yam.codes>
2024-09-12 00:10:16 +0500

    fix: benchmarks to use spread input

--- benchmarks/array/cartesianProduct.bench.ts
+++ benchmarks/array/cartesianProduct.bench.ts
@@ -2,29 +2,22 @@ import * as _ from 'radashi'
 
 describe('cartesianProduct', () => {
   bench('with no arguments', () => {
-    _.cartesianProduct([])
+    _.cartesianProduct()
   })
 
   bench('with single empty array', () => {
-    _.cartesianProduct([[]])
+    _.cartesianProduct([])
   })
 
   bench('with one non-empty array (n=1)', () => {
-    _.cartesianProduct([['a', 'b', 'c']])
+    _.cartesianProduct(['a', 'b', 'c'])
   })
 
   bench('with two small arrays (n=2)', () => {
-    _.cartesianProduct([
-      ['red', 'blue'],
-      ['fast', 'slow'],
-    ])
+    _.cartesianProduct(['red', 'blue'], ['fast', 'slow'])
   })
 
   bench('with three small arrays (n=3)', () => {
-    _.cartesianProduct([
-      ['red', 'blue'],
-      ['fast', 'slow'],
-      ['big', 'small'],
-    ])
+    _.cartesianProduct(['red', 'blue'], ['fast', 'slow'], ['big', 'small'])
   })
 })
