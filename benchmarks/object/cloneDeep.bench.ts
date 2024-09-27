2a90380d98f7112d489480c12a5451069f28697b
marlonpassos <marlon.passos@cadastra.com>
2024-09-27 16:43:16 +0000

    fix: type error

--- benchmarks/object/cloneDeep.bench.ts
+++ benchmarks/object/cloneDeep.bench.ts
@@ -1,7 +1,7 @@
 import * as _ from 'radashi'
 
 describe('cloneDeep', () => {
-  const objects: any = _.list(0, 5, i => {
+  const objects: any = _.list<number>(0, 5, i => {
     const object: any = {}
     _.set(object, 'a.b.c.d.e.f.g.h.i.k.l.m.n.o.p', i)
     return object
