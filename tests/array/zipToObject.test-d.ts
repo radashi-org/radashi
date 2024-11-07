7458cbd89fdb91d0438bc6b8b354ee49248ae1a4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-07 16:16:21 -0500

    wrap type test in describe block

--- tests/array/zipToObject.test-d.ts
+++ tests/array/zipToObject.test-d.ts
@@ -1,10 +1,12 @@
 import * as _ from 'radashi'
 
-test('readonly arrays', () => {
-  const names = ['ra', 'zeus', 'loki'] as const
-  const cultures = ['egypt', 'greek', 'norse'] as const
+describe('zipToObject', () => {
+  test('readonly arrays', () => {
+    const names = ['ra', 'zeus', 'loki'] as const
+    const cultures = ['egypt', 'greek', 'norse'] as const
 
-  expectTypeOf(_.zipToObject(names, cultures)).toEqualTypeOf<
-    Record<'ra' | 'zeus' | 'loki', 'egypt' | 'greek' | 'norse'>
-  >()
+    expectTypeOf(_.zipToObject(names, cultures)).toEqualTypeOf<
+      Record<'ra' | 'zeus' | 'loki', 'egypt' | 'greek' | 'norse'>
+    >()
+  })
 })
