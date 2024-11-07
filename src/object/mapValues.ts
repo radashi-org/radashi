2ade9bec805c3612e5bb08c7edd025af51a2dad5
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-07 16:12:26 -0500

    fix(types): `mapValues` index signature handling (#297)
    …and optional properties handling.

--- src/object/mapValues.ts
+++ src/object/mapValues.ts
@@ -10,42 +10,15 @@
  * ```
  * @version 12.1.0
  */
-export function mapValues<
-  TValue,
-  TKey extends string | number | symbol,
-  TNewValue,
->(
-  obj: { [K in TKey]: TValue },
-  mapFunc: (value: TValue, key: TKey) => TNewValue,
-): { [K in TKey]: TNewValue }
-
-// This overload exists to support cases where `obj` is a partial
-// object whose values are never undefined when the key is also
-// defined. For example:
-//   { [key: string]?: number } versus { [key: string]: number | undefined }
-export function mapValues<
-  TValue,
-  TKey extends string | number | symbol,
-  TNewValue,
->(
-  obj: { [K in TKey]?: TValue },
-  mapFunc: (value: TValue, key: TKey) => TNewValue,
-): { [K in TKey]?: TNewValue }
-
-export function mapValues<
-  TValue,
-  TKey extends string | number | symbol,
-  TNewValue,
->(
-  obj: { [K in TKey]?: TValue },
-  mapFunc: (value: TValue, key: TKey) => TNewValue,
-): Record<TKey, TNewValue> {
-  const keys = Object.keys(obj) as TKey[]
-  return keys.reduce(
+export function mapValues<T extends object, U>(
+  obj: T,
+  mapFunc: (value: Required<T>[keyof T], key: keyof T) => U,
+): { [K in keyof T]: U } {
+  return (Object.keys(obj) as (keyof T)[]).reduce(
     (acc, key) => {
-      acc[key] = mapFunc(obj[key]!, key)
+      acc[key] = mapFunc(obj[key], key)
       return acc
     },
-    {} as Record<TKey, TNewValue>,
+    {} as { [K in keyof T]: U },
   )
 }
