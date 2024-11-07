762d1079e9e64c19df0d94efd04bfe1fc5063117
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-07 16:06:52 -0500

    fix optional properties

--- tests/object/mapValues.test-d.ts
+++ tests/object/mapValues.test-d.ts
@@ -2,22 +2,28 @@ import { mapValues } from 'radashi'
 
 describe('mapValues', () => {
   test('Record types', () => {
-    const object = mapValues({} as Record<string, number>, x => String(x))
+    const object = mapValues({} as Record<string, number>, x => x.toFixed(2))
     expectTypeOf(object).toEqualTypeOf<Record<string, string>>()
   })
 
   test('index signature types', () => {
-    const object = mapValues({} as { [key: string]: number }, x => String(x))
+    const object = mapValues({} as { [key: string]: number }, x => x.toFixed(2))
     expectTypeOf(object).toEqualTypeOf<{ [key: string]: string }>()
   })
 
   test('literal types', () => {
-    const object = mapValues({} as { a: number }, x => String(x))
+    const object = mapValues({} as { a: number }, x => x.toFixed(2))
     expectTypeOf(object).toEqualTypeOf<{ a: string }>()
   })
 
   test('optional types', () => {
-    const object = mapValues({} as { a?: number }, x => String(x))
+    const object = mapValues({} as { a?: number }, x => {
+      // Due to exactOptionalPropertyTypes, the type-checker knows
+      // that `x` will never be undefined, because `mapValues` can
+      // only process keys that are present.
+      expectTypeOf(x).toEqualTypeOf<number>()
+      return x.toFixed(2)
+    })
     expectTypeOf(object).toEqualTypeOf<{ a?: string }>()
   })
 })
