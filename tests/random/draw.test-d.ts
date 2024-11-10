08358524c34eb762ef0fea5b9d36a19a589aebed
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-10 09:23:57 -0500

    refactor type tests

--- tests/random/draw.test-d.ts
+++ tests/random/draw.test-d.ts
@@ -1,27 +1,31 @@
 import * as _ from 'radashi'
 
-describe('draw types', () => {
-  test('return type with literal argument', () => {
-    expectTypeOf(_.draw([])).toBeNull()
-    expectTypeOf(_.draw([1, 2, 3])).toBeNumber()
-  })
-  test('return type with mutable variable', () => {
-    const neverList: never[] = []
+describe('draw', () => {
+  test('variable with mutable array', () => {
     const emptyList: number[] = []
     const filledList = [1, 2, 3]
 
-    expectTypeOf(_.draw(neverList)).toEqualTypeOf<null>()
     expectTypeOf(_.draw(emptyList)).toEqualTypeOf<number | null>()
     expectTypeOf(_.draw(filledList)).toEqualTypeOf<number | null>()
   })
-  test('return type with immutable variable', () => {
-    const neverList: never[] = [] as const
-    const emptyList: number[] = [] as const
+
+  test('variable with empty array', () => {
+    const neverList: never[] = []
+    const emptyList = [] as const
+
+    expectTypeOf(_.draw(neverList)).toEqualTypeOf<null>()
+    expectTypeOf(_.draw(emptyList)).toEqualTypeOf<null>()
+  })
+
+  test('variable with tuple', () => {
     const filledList = [1, 2, 3] as const
 
-    expectTypeOf(_.draw(neverList)).toBeNull()
-    // FIXME: Can this be narrowed to `null`?
-    expectTypeOf(_.draw(emptyList)).toEqualTypeOf<number | null>()
     expectTypeOf(_.draw(filledList)).toEqualTypeOf<1 | 2 | 3>()
+  })
+
+  test('inlined array', () => {
+    expectTypeOf(_.draw([])).toEqualTypeOf<null>()
+    expectTypeOf(_.draw([1, 2, 3])).toEqualTypeOf<number>()
+    expectTypeOf(_.draw([1, 2, 3] as const)).toEqualTypeOf<1 | 2 | 3>()
   })
 })
