0ef8e44d682eb443540335aed1815ed1322bd161
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-11 17:01:59 -0500

    chore: update scripts/checkout-pr.sh

--- scripts/checkout-pr.sh
+++ scripts/checkout-pr.sh
@@ -12,10 +12,13 @@ BASE_COMMIT=$(git merge-base HEAD "pr/$PR_HEAD_REF")
 git checkout "pr/$PR_HEAD_REF"
 git reset --soft "$BASE_COMMIT"
 git commit -m "single commit"
+SQUASH_COMMIT=$(git rev-parse HEAD)
+
+# Return to the original branch
 git checkout -
 
 # Import changes from the PR into the current branch without committing
-git cherry-pick -m 1 -n "pr/$PR_HEAD_REF"
+git cherry-pick -m 1 -n "$SQUASH_COMMIT"
 
 # List the affected files for debugging purposes
 git diff --name-status --staged
