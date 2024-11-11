abea092efdd50a64e05afb810fedd8e4b8ee25f9
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-11 16:58:42 -0500

    chore: update scripts/checkout-pr.sh

--- scripts/checkout-pr.sh
+++ scripts/checkout-pr.sh
@@ -6,9 +6,16 @@ set -x
 git remote add pr $PR_REPO_URL
 git fetch pr "$PR_HEAD_REF"
 
-# Import changes from the PR into the current branch without committing
 BASE_COMMIT=$(git merge-base HEAD "pr/$PR_HEAD_REF")
-git cherry-pick -m 1 -n "$BASE_COMMIT..pr/$PR_HEAD_REF"
+
+# Squash the PR into a single commit
+git checkout "pr/$PR_HEAD_REF"
+git reset --soft "$BASE_COMMIT"
+git commit -m "single commit"
+git checkout -
+
+# Import changes from the PR into the current branch without committing
+git cherry-pick -m 1 -n "pr/$PR_HEAD_REF"
 
 # List the affected files for debugging purposes
 git diff --name-status --staged
