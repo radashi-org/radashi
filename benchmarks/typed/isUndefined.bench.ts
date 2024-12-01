228580d7f289889db64490f419e443691d2bd8da
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-12-01 10:28:14 -0500

    remove unnecessary benchmarks

--- benchmarks/typed/isUndefined.bench.ts
+++ benchmarks/typed/isUndefined.bench.ts
@@ -1,23 +1,5 @@
 import * as _ from 'radashi'
 
-describe('isUndefined', () => {
-  bench('with null', () => {
-    _.isUndefined(null)
-  })
-
-  bench('with undefined', () => {
-    _.isUndefined(undefined)
-  })
-
-  bench('with string', () => {
-    _.isUndefined('abc')
-  })
-
-  bench('with number', () => {
-    _.isUndefined(22)
-  })
-
-  bench('with object', () => {
-    _.isUndefined({})
-  })
+bench('isUndefined', () => {
+  _.isUndefined(undefined)
 })
