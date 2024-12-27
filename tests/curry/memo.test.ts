77932b601228b588dc59fcd5bf5a0cbeb7caf2db
hugofqt <12221094+hugo082@users.noreply.github.com>
2024-12-27 10:58:08 +0100

    feat: dissociate memo chache read keys from set keys

--- tests/curry/memo.test.ts
+++ tests/curry/memo.test.ts
@@ -7,6 +7,7 @@ describe('memo', () => {
     const resultB = func()
     expect(resultA).toBe(resultB)
   })
+
   test('uses key to identify unique calls', () => {
     const func = _.memo(
       (arg: { user: { id: string } }) => {
@@ -23,6 +24,32 @@ describe('memo', () => {
     expect(resultA).toBe(resultA2)
     expect(resultB).not.toBe(resultA)
   })
+
+  test('uses multiple keys to identify unique calls', () => {
+    const rawFn = vi.fn((arg: { id: string; withAdditionalStuff: boolean }) => {
+      if (arg.withAdditionalStuff) {
+        // do stuff
+      }
+
+      return arg.id
+    })
+
+    const func = _.memo(rawFn, {
+      key: arg =>
+        arg.withAdditionalStuff
+          ? `${arg.id}_withAdditionalStuff`
+          : [`${arg.id}`, `${arg.id}_withAdditionalStuff`], // we also look for the shared key
+      setKey: arg =>
+        arg.withAdditionalStuff
+          ? [`${arg.id}`, `${arg.id}_withAdditionalStuff`] // we also set the shared key
+          : `${arg.id}`,
+    })
+
+    func({ id: '1', withAdditionalStuff: true })
+    func({ id: '1', withAdditionalStuff: false })
+    expect(rawFn).toHaveBeenCalledTimes(1)
+  })
+
   test('calls function again when first value expires', async () => {
     vi.useFakeTimers()
     const func = _.memo(() => new Date().getTime(), {
@@ -33,6 +60,7 @@ describe('memo', () => {
     const resultB = func()
     expect(resultA).not.toBe(resultB)
   })
+
   test('does not call function again when first value has not expired', async () => {
     vi.useFakeTimers()
     const func = _.memo(() => new Date().getTime(), {
