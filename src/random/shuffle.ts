16fec8e2c4ad6e00ad87369bd79f30df7c8d870b
Christian Rishøj <christian@rishoj.net>
2025-01-10 02:41:49 +0700

    fix: only pick from the unshuffled portion

--- src/random/shuffle.ts
+++ src/random/shuffle.ts
@@ -38,11 +38,11 @@ export function shuffle<T>(
   random: (min: number, max: number) => number = _.random,
 ): T[] {
   const newArray = array.slice()
-  for (let idx = 0, randomIdx: number, item: T; idx < array.length; idx++) {
-    randomIdx = random(0, array.length - 1)
-    item = newArray[randomIdx]
-    newArray[randomIdx] = newArray[idx]
-    newArray[idx] = item
+  for (let idx = array.length - 1, randomIdx: number, item: T; idx > 0; idx--) {
+    randomIdx = random(0, idx)
+    item = newArray[idx]
+    newArray[idx] = newArray[randomIdx]
+    newArray[randomIdx] = item
   }
   return newArray
 }
