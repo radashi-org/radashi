e460d62daeee4cef169086ba73dc873da0affb43
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-05 17:10:41 -0500

    add Object.keys benchmark comparison

--- benchmarks/object/isEmptyRecord.bench.ts
+++ benchmarks/object/isEmptyRecord.bench.ts
@@ -14,3 +14,13 @@ describe('isEmptyRecord', () => {
     isEmptyRecord({ a: 1, b: 2, c: 3 })
   })
 })
+
+describe('Object.keys comparison', () => {
+  bench('empty object', () => {
+    Object.keys({}).length === 0
+  })
+
+  bench('object with one property', () => {
+    Object.keys({ a: 1 }).length === 0
+  })
+})
