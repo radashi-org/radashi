f65a347a0aa8142fcfa59b2a84c3090e19b0f96b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-03-13 13:12:56 -0400

    update tests

--- tests/curry/memoLastCall.test.ts
+++ tests/curry/memoLastCall.test.ts
@@ -2,9 +2,9 @@ import * as _ from 'radashi'
 
 describe('memoLastCall', () => {
   test('caches the last result', () => {
-    let calculationCount = 0
+    let callCount = 0
     const expensiveCalculation = (x: number, y: number): { result: number } => {
-      calculationCount++
+      callCount++
       return { result: x + y }
     }
 
@@ -12,33 +12,37 @@ describe('memoLastCall', () => {
 
     const firstCall = memoizedCalc(2, 3)
     expect(firstCall).toEqual({ result: 5 })
-    expect(calculationCount).toBe(1)
+    expect(callCount).toBe(1)
 
-    const secondCall = memoizedCalc(2, 3)
-    expect(secondCall).toBe(firstCall) // Check memoization
-    expect(calculationCount).toBe(1)
+    let nextCall = memoizedCalc(2, 3)
+    expect(nextCall).toBe(firstCall) // Check memoization
+    expect(callCount).toBe(1)
 
-    const thirdCall = memoizedCalc(3, 4)
-    expect(thirdCall).toEqual({ result: 7 })
-    expect(thirdCall).not.toBe(firstCall) // Check cache was overwritten
-    expect(calculationCount).toBe(2)
+    nextCall = memoizedCalc(3, 4)
+    expect(nextCall).toEqual({ result: 7 })
+    expect(nextCall).not.toBe(firstCall) // Check cache was overwritten
+    expect(callCount).toBe(2)
 
-    const fourthCall = memoizedCalc(2, 3)
-    expect(fourthCall).toEqual({ result: 5 })
-    expect(fourthCall).not.toBe(firstCall) // Check previous cache was overwritten
-    expect(calculationCount).toBe(3)
+    nextCall = memoizedCalc(2, 3)
+    expect(nextCall).toEqual({ result: 5 })
+    expect(nextCall).not.toBe(firstCall) // Check previous cache was overwritten
+    expect(callCount).toBe(3)
   })
 
   test('handles different argument types', () => {
     const memoizedFn = _.memoLastCall((a: string, b: number, c: boolean) => ({
       value: `${a}-${b}-${c}`,
     }))
+
     const firstCall = memoizedFn('foo', 123, true)
     expect(firstCall).toEqual({ value: 'foo-123-true' })
-    expect(memoizedFn('foo', 123, true)).toBe(firstCall)
-    const secondCall = memoizedFn('bar', 456, false)
-    expect(secondCall).toEqual({ value: 'bar-456-false' })
-    expect(secondCall).not.toBe(firstCall)
+
+    let nextCall = memoizedFn('foo', 123, true)
+    expect(nextCall).toBe(firstCall)
+
+    nextCall = memoizedFn('bar', 456, false)
+    expect(nextCall).toEqual({ value: 'bar-456-false' })
+    expect(nextCall).not.toBe(firstCall)
   })
 
   test('handles functions with no arguments', () => {
@@ -47,9 +51,11 @@ describe('memoLastCall', () => {
       callCount++
       return { value: 'result' + callCount }
     })
+
     const firstCall = memoizedFn()
     expect(firstCall).toEqual({ value: 'result1' })
     expect(callCount).toBe(1)
+
     expect(memoizedFn()).toBe(firstCall)
     expect(callCount).toBe(1)
   })
