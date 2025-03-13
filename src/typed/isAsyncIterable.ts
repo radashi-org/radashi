a88cf09a20f7c0c55abadd93f1ec9c2a740bfd8d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-03-13 11:50:41 -0400

    handle missing Symbol.asyncIterator

--- src/typed/isAsyncIterable.ts
+++ src/typed/isAsyncIterable.ts
@@ -1,3 +1,11 @@
+// Whenever Symbol.asyncIterator is missing, use the same symbol that
+// polyfills do (e.g. @azure/core-asynciterator-polyfill).
+const asyncIteratorSymbol: symbol =
+  (Symbol as any).asyncIterator || Symbol.for('Symbol.asyncIterator')
+
+// @ts-ignore: Assume "lib.es2018.asynciterable" is included.
+type AsyncIterable = globalThis.AsyncIterable<unknown>
+
 /**
  * Checks if a value is an async iterable.
  *
@@ -13,12 +21,10 @@
  * @param value The value to check.
  * @returns `true` if the value is an async iterable, `false` otherwise.
  */
-export function isAsyncIterable(value: unknown): // @ts-ignore
-value is globalThis.AsyncIterable<unknown> {
+export function isAsyncIterable(value: unknown): value is AsyncIterable {
   return (
     !!value &&
     typeof value === 'object' &&
-    // @ts-ignore
-    Symbol.asyncIterator in value
+    typeof value[asyncIteratorSymbol as never] === 'function'
   )
 }
