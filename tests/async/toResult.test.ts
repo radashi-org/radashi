d6299eb7d3e6e3afd209e6295f07676597fc640b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-03-13 14:08:49 -0400

    add test case

--- tests/async/toResult.test.ts
+++ tests/async/toResult.test.ts
@@ -11,7 +11,7 @@ test('converts a rejected promise to Err', async () => {
   const promise = Promise.reject(new Error('test error'))
   const result = await _.toResult(promise)
   expect(result[0]).toBeInstanceOf(Error)
-  expect((result[0] as Error).message).toEqual('test error')
+  expect(result[0]!.message).toEqual('test error')
   expect(result[1]).toBeUndefined()
 })
 
@@ -19,6 +19,22 @@ test('converts a rejected promise with a non-error to Err', async () => {
   const promise = Promise.reject('test error')
   const result = await _.toResult(promise)
   expect(result[0]).toBeInstanceOf(Error)
-  expect((result[0] as Error).message).toEqual('test error')
+  expect(result[0]!.message).toEqual('test error')
   expect(result[1]).toBeUndefined()
+})
+
+test('does not catch synchronous errors', () => {
+  const shouldThrow = true
+  expect(() => {
+    const fn = () => {
+      if (shouldThrow) {
+        // This error is thrown synchronously, not as a rejected promise
+        throw new Error('synchronous error')
+      }
+      return Promise.resolve(1)
+    }
+
+    // The following line will throw immediately, not returning a Result
+    _.toResult(fn())
+  }).toThrow('synchronous error')
 })
