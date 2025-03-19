3bd0b203d60378c6c9ce5a839d413faef29540a7
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-03-19 12:51:04 -0400

    tweak benchmarks

--- benchmarks/array/cluster.bench.ts
+++ benchmarks/array/cluster.bench.ts
@@ -1,13 +1,13 @@
 import * as _ from 'radashi'
 
 describe('cluster', () => {
-  bench('with cluster size of 2', () => {
-    const list = [1, 1, 1, 1, 1, 1, 1, 1]
+  bench('small array', () => {
+    const list = new Array(10).fill(1)
     _.cluster(list, 2)
   })
 
-  bench('specified cluster size of 3', () => {
-    const list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]
-    _.cluster(list, 3)
+  bench('large array', () => {
+    const list = new Array(10000).fill(1)
+    _.cluster(list, 4)
   })
 })
