167d104021f056eb530ca791d8f4d33e7c338d7a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-03-22 23:28:18 -0400

    update benchmarks

--- benchmarks/array/pluck.bench.ts
+++ benchmarks/array/pluck.bench.ts
@@ -1,33 +1,32 @@
 import * as _ from 'radashi'
 
 describe('pluck', () => {
-  bench('plucking a single property from a small array', () => {
-    const gods = [
-      { name: 'Ra', power: 100, domain: 'Sun' },
-      { name: 'Zeus', power: 98, domain: 'Lightning' },
-      { name: 'Loki', power: 72, domain: 'Tricks' },
-    ]
-    _.pluck(gods, 'name')
+  const smallArray = [
+    { name: 'Ra', power: 100, domain: 'Sun' },
+    { name: 'Zeus', power: 98, domain: 'Lightning' },
+    { name: 'Loki', power: 72, domain: 'Tricks' },
+  ]
+
+  const largeArray = Array.from({ length: 1000 }, (_, idx) => ({
+    id: idx,
+    name: `God${idx}`,
+    power: Math.floor(Math.random() * 100),
+    domain: `Domain${idx}`,
+  }))
+
+  bench('single property from a small array', () => {
+    _.pluck(smallArray, ['name'])
   })
 
-  bench('plucking multiple properties from a list of objects', () => {
-    const gods = [
-      { name: 'Ra', power: 100, domain: 'Sun' },
-      { name: 'Zeus', power: 98, domain: 'Lightning' },
-      { name: 'Loki', power: 72, domain: 'Tricks' },
-    ]
-    _.pluck(gods, 'power', 'domain')
+  bench('multiple properties from a small array', () => {
+    _.pluck(smallArray, ['power', 'domain'])
   })
 
-  bench('plucking from a large array with multiple keys', () => {
-    const largeArray = Array(1000)
-      .fill(null)
-      .map((_, idx) => ({
-        id: idx,
-        name: `God${idx}`,
-        power: Math.floor(Math.random() * 100),
-        domain: `Domain${idx}`,
-      }))
-    _.pluck(largeArray, 'name', 'power')
+  bench('all properties from a small array', () => {
+    _.pluck(smallArray)
+  })
+
+  bench('multiple properties from a large array', () => {
+    _.pluck(largeArray, ['name', 'power'])
   })
 })
