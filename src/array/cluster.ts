b76ba14c615f99435918bb5371fa95bcdbb721d8
fResult <styxmaz@gmail.com>
2025-04-14 01:43:00 +0700

    Improve the `cluster` function can infer type as `[T, T, ..., T][]`.

--- src/array/cluster.ts
+++ src/array/cluster.ts
@@ -9,10 +9,12 @@
  * ```
  * @version 12.1.0
  */
-export function cluster<T>(array: readonly T[], size = 2): T[][] {
-  const clusters: T[][] = []
+export function cluster<T, N extends number = 2>(array: readonly T[], size: N = 2 as N): UniformTuple<T, N>[] {
+  const clusters: UniformTuple<T, N>[] = []
   for (let i = 0; i < array.length; i += size) {
-    clusters.push(array.slice(i, i + size))
+    clusters.push(array.slice(i, i + size) as UniformTuple<T, N>)
   }
   return clusters
 }
+
+type UniformTuple<T, N extends number, R extends T[] = []> = R['length'] extends N ? R : UniformTuple<T, N, [T, ...R]>
