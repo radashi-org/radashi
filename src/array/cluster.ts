329522567c5e2f41f748ca8545963e2d31665104
fResult <styxmaz@gmail.com>
2025-04-14 02:17:09 +0700

    Fix code format violation.

--- src/array/cluster.ts
+++ src/array/cluster.ts
@@ -20,12 +20,20 @@ export function cluster<T, N extends number = 2>(
   return clusters as any
 }
 
-type UniformTuple<T, N extends number> = N extends 1 ? [T]
-  : N extends 2 ? [T, T]
-  : N extends 3 ? [T, T, T]
-  : N extends 4 ? [T, T, T, T]
-  : N extends 5 ? [T, T, T, T, T]
-  : N extends 6 ? [T, T, T, T, T, T]
-  : N extends 7 ? [T, T, T, T, T, T, T]
-  : N extends 8 ? [T, T, T, T, T, T, T, T]
-  : T[]
+type UniformTuple<T, N extends number> = N extends 1
+  ? [T]
+  : N extends 2
+    ? [T, T]
+    : N extends 3
+      ? [T, T, T]
+      : N extends 4
+        ? [T, T, T, T]
+        : N extends 5
+          ? [T, T, T, T, T]
+          : N extends 6
+            ? [T, T, T, T, T, T]
+            : N extends 7
+              ? [T, T, T, T, T, T, T]
+              : N extends 8
+                ? [T, T, T, T, T, T, T, T]
+                : T[]
