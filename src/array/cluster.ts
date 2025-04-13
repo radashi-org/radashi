03feb3eb03ce92d426712c264cc43937aa581ae9
fResult <styxmaz@gmail.com>
2025-04-14 02:02:24 +0700

    Make `cluster`'s type inference supports common size and fallback.
    - Common sizes are 1-8 of `UniformTuple<T>[]`
    - Fallback with `T[][]`

--- src/array/cluster.ts
+++ src/array/cluster.ts
@@ -9,12 +9,23 @@
  * ```
  * @version 12.1.0
  */
-export function cluster<T, N extends number = 2>(array: readonly T[], size: N = 2 as N): UniformTuple<T, N>[] {
-  const clusters: UniformTuple<T, N>[] = []
+export function cluster<T, N extends number = 2>(
+  array: readonly T[],
+  size: N = 2 as N,
+): UniformTuple<T, N>[] {
+  const clusters: T[][] = []
   for (let i = 0; i < array.length; i += size) {
-    clusters.push(array.slice(i, i + size) as UniformTuple<T, N>)
+    clusters.push(array.slice(i, i + size) as any)
   }
-  return clusters
+  return clusters as any
 }
 
-type UniformTuple<T, N extends number, R extends T[] = []> = R['length'] extends N ? R : UniformTuple<T, N, [T, ...R]>
+type UniformTuple<T, N extends number> = N extends 1 ? [T]
+  : N extends 2 ? [T, T]
+  : N extends 3 ? [T, T, T]
+  : N extends 4 ? [T, T, T, T]
+  : N extends 5 ? [T, T, T, T, T]
+  : N extends 6 ? [T, T, T, T, T, T]
+  : N extends 7 ? [T, T, T, T, T, T, T]
+  : N extends 8 ? [T, T, T, T, T, T, T, T]
+  : T[]
