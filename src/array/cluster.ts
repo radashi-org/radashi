45c5017c2f0b432cc1b62aad1e5198043ba9da64
fResult <styxmaz@gmail.com>
2025-04-14 02:49:00 +0700

    Update types in `cluster` function.

--- src/array/cluster.ts
+++ src/array/cluster.ts
@@ -13,11 +13,11 @@ export function cluster<T, N extends number = 2>(
   array: readonly T[],
   size: N = 2 as N,
 ): UniformTuple<T, N>[] {
-  const clusters: T[][] = []
+  const clusters: UniformTuple<T, N>[] = []
   for (let i = 0; i < array.length; i += size) {
-    clusters.push(array.slice(i, i + size) as any)
+    clusters.push(array.slice(i, i + size) as UniformTuple<T, N>)
   }
-  return clusters as any
+  return clusters
 }
 
 type UniformTuple<T, N extends number> = N extends 1
