6b85d670de3b4004b2dc924624a178b311daf280
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-04-15 17:42:16 -0400

    chore: minor tweaks

--- src/array/cluster.ts
+++ src/array/cluster.ts
@@ -9,31 +9,31 @@
  * ```
  * @version 12.1.0
  */
-export function cluster<T, N extends number = 2>(
+export function cluster<T, Size extends number = 2>(
   array: readonly T[],
-  size: N = 2 as N,
-): UniformTuple<T, N>[] {
+  size: Size = 2 as Size,
+): Cluster<T, Size>[] {
   const clusters: T[][] = []
   for (let i = 0; i < array.length; i += size) {
     clusters.push(array.slice(i, i + size))
   }
-  return clusters as UniformTuple<T, N>[]
+  return clusters as Cluster<T, Size>[]
 }
 
-type UniformTuple<T, N extends number> = N extends 1
+type Cluster<T, Size extends number> = Size extends 1
   ? [T]
-  : N extends 2
+  : Size extends 2
     ? [T, T]
-    : N extends 3
+    : Size extends 3
       ? [T, T, T]
-      : N extends 4
+      : Size extends 4
         ? [T, T, T, T]
-        : N extends 5
+        : Size extends 5
           ? [T, T, T, T, T]
-          : N extends 6
+          : Size extends 6
             ? [T, T, T, T, T, T]
-            : N extends 7
+            : Size extends 7
               ? [T, T, T, T, T, T, T]
-              : N extends 8
+              : Size extends 8
                 ? [T, T, T, T, T, T, T, T]
                 : T[]
