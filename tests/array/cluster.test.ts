5944824ae01c4150ee3db329b1fa0c5d0806f309
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-05-15 01:09:56 -0400

    chore(test): style tweaks and remove one test

--- tests/array/cluster.test.ts
+++ tests/array/cluster.test.ts
@@ -2,69 +2,50 @@ import * as _ from 'radashi'
 
 describe('cluster', () => {
   test('returns an array of arrays', () => {
-    const list = [1, 1, 1, 1, 1, 1, 1, 1]
-    const result = _.cluster(list)
-    const [a, b, c] = result
-    expect(a).toEqual([1, 1])
-    expect(b).toEqual([1, 1])
-    expect(c).toEqual([1, 1])
+    const data = [1, 1, 1, 1, 1, 1, 1, 1]
+    expect(_.cluster(data, 2)).toEqual([
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+    ])
   })
 
   test('returns remainder in final cluster', () => {
-    const list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]
-    const result = _.cluster(list, 3)
-    const [a, b, c, d] = result
-    expect(a).toEqual([1, 1, 1])
-    expect(b).toEqual([1, 1, 1])
-    expect(c).toEqual([1, 1, 1])
-    expect(d).toEqual([2, 2])
+    const data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]
+    expect(_.cluster(data, 3)).toEqual([
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+      [2, 2],
+    ])
+  })
+
+  test('uses default size of 2 when no size provided', () => {
+    expect(_.cluster([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4], [5]])
   })
 
   test('handles empty array', () => {
-    const result = _.cluster([], 2);
-    expect(result).toEqual([]);
-  });
+    expect(_.cluster([], 2)).toEqual([])
+  })
 
   test('creates individual clusters when size is 1', () => {
-    const list = [1, 2, 3, 4];
-    const result = _.cluster(list, 1);
-    expect(result).toEqual([[1], [2], [3], [4]]);
-  });
-
-  test('creates a single cluster when size exceeds array length', () => {
-    const list = [1, 2, 3];
-    const result = _.cluster(list, 5);
-    expect(result).toEqual([[1, 2, 3]]);
-  });
+    expect(_.cluster([1, 2, 3, 4], 1)).toEqual([[1], [2], [3], [4]])
+  })
 
   test('returns empty list when size <= 0', () => {
-    const expectedResult: number[] = []
-    const list = [1, 2, 3, 4];
+    const data = [1, 2, 3, 4]
 
-    const actualResult1 = _.cluster(list, 0);
-    const actualResult2 = _.cluster(list, -1);
-    const actualResult3 = _.cluster(list, Number.NEGATIVE_INFINITY);
+    expect(_.cluster(data, 0)).toEqual([])
+    expect(_.cluster(data, -1)).toEqual([])
+    expect(_.cluster(data, Number.NEGATIVE_INFINITY)).toEqual([])
+  })
 
-    expect(actualResult1).toEqual(expectedResult);
-    expect(actualResult2).toEqual(expectedResult);
-    expect(actualResult3).toEqual(expectedResult);
-  });
-
-  test('uses default size of 2 when no size provided', () => {
-    const list = [1, 2, 3, 4, 5];
-    const actualResult = _.cluster(list);
-    expect(actualResult).toEqual([[1, 2], [3, 4], [5]]);
-  });
-
-  test('handles arrays with mixed data types', () => {
-    const list = [1, 'a', true, {}, null];
-    const actualResult = _.cluster(list, 2);
-    expect(actualResult).toEqual([[1, 'a'], [true, {}], [null]]);
-  });
+  test('creates a single cluster when size exceeds array length', () => {
+    expect(_.cluster([1, 2, 3], 5)).toEqual([[1, 2, 3]])
+  })
 
   test('handles very large size values', () => {
-    const list = [1, 2, 3];
-    const actualResult = _.cluster(list, Number.MAX_SAFE_INTEGER);
-    expect(actualResult).toEqual([[1, 2, 3]]);
-  });
+    expect(_.cluster([1, 2, 3], Number.MAX_SAFE_INTEGER)).toEqual([[1, 2, 3]])
+  })
 })
