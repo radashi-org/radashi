50646714a861621d1e8604e1326eb8122a8e567c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-06-28 14:05:16 -0400

    feat: check for reference equality

--- src/array/arrayEquals.ts
+++ src/array/arrayEquals.ts
@@ -15,12 +15,14 @@
  * @version 12.7.0
  */
 export function arrayEquals<T>(array1: T[], array2: T[]): boolean {
-  if (array1.length !== array2.length) {
-    return false
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (!Object.is(array1[i], array2[i])) {
+  if (array1 !== array2) {
+    if (array1.length !== array2.length) {
       return false
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (!Object.is(array1[i], array2[i])) {
+        return false
+      }
     }
   }
   return true
