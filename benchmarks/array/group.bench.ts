ae85f76dc8d33f5216be1a7739879590f0a08966
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-09 01:20:33 -0400

    chore(perf): add temporary benchmark for further comparison

--- benchmarks/array/group.bench.ts
+++ benchmarks/array/group.bench.ts
@@ -11,4 +11,34 @@ describe('group', () => {
     ]
     _.group(list, x => x.group)
   })
+
+  describe('old version', () => {
+    bench('with groups by provided attribute', () => {
+      const list = [
+        { group: 'a', word: 'hello' },
+        { group: 'b', word: 'bye' },
+        { group: 'a', word: 'oh' },
+        { group: 'b', word: 'hey' },
+        { group: 'c', word: 'ok' },
+      ]
+      group(list, x => x.group)
+    })
+
+    function group<T, Key extends string | number | symbol>(
+      array: readonly T[],
+      getGroupId: (item: T, index: number) => Key,
+    ): { [K in Key]?: T[] } {
+      return array.reduce(
+        (acc, item, index) => {
+          const groupId = getGroupId(item, index)
+          if (!acc[groupId]) {
+            acc[groupId] = []
+          }
+          acc[groupId].push(item)
+          return acc
+        },
+        Object.create(null) as Record<Key, T[]>,
+      )
+    }
+  })
 })
