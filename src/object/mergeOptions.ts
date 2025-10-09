febadf7bd665ad993ff13d365dca0f3fa86793a4
fResult <styxmaz@gmail.com>
2025-10-09 22:51:41 +0700

    feat(mergeOptions): create `mergeOptions` function and its type

---
+++ src/object/mergeOptions.ts
@@ -0,0 +1,58 @@
+type NonNull<T> = {} & T
+
+type Expand<T> = T extends object
+  ? {
+      [K in keyof T]: T[K] extends string
+        ? string
+        : T[K] extends number
+          ? number
+          : T[K]
+    }
+  : T extends string
+    ? string
+    : T extends number
+      ? number
+      : T
+
+type UndefinedToPartial<T> = T extends object
+  ? undefined extends T
+    ? Partial<Omit<T, undefined>>
+    : T
+  : T
+
+type MergeObjects<A extends object, B extends object> = {
+  [K in keyof A | keyof B]: K extends keyof B
+    ? B[K]
+    : K extends keyof A
+      ? A[K]
+      : never
+}
+
+export type MergeOptions<
+  A extends object | undefined,
+  B extends object | undefined,
+> = [A, B] extends [undefined, undefined]
+  ? undefined
+  : [A] extends [undefined]
+    ? Expand<NonNull<B>>
+    : [B] extends [undefined]
+      ? Expand<NonNull<A>>
+      : Expand<MergeObjects<UndefinedToPartial<NonNullable<A>>, NonNullable<B>>>
+
+export function mergeOptions<
+  A extends object | undefined,
+  B extends object | undefined,
+>(a: A, b: B): MergeOptions<A, B>
+
+export function mergeOptions(
+  a: object | undefined,
+  b: object | undefined,
+): object | undefined {
+  if (a === undefined) {
+    return b
+  }
+  if (b === undefined) {
+    return a
+  }
+  return { ...a, ...b }
+}
